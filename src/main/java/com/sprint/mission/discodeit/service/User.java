29859019947d75825d1d2d037787583fe3a8e283
bustam00 <bustam080@gmail.com>
2025-07-11 15:14:51 +0900

    feat: add User.java

---
+++ src/main/java/com/sprint/mission/discodeit/service/User.java
@@ -0,0 +1,26 @@
+package com.sprint.mission.discodeit.service;
+
+import java.util.UUID;
+
+public class User {
+    private UUID id;
+
+    public User(UUID id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("User{");
+        sb.append("id=").append(id);
+        sb.append('}');
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+
+
+}
