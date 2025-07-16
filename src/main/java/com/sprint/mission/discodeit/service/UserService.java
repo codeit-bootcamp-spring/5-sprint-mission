07b11b28f3f84be7393faffec9e348dd0fc08759
sooyeonz <5196ksy@naver.com>
2025-07-16 21:57:01 +0900

    feat: delete nickname field

--- src/main/java/com/sprint/mission/discodeit/service/UserService.java
+++ src/main/java/com/sprint/mission/discodeit/service/UserService.java
@@ -6,13 +6,13 @@ import java.util.List;
 import java.util.UUID;
 
 public interface UserService {
-    User create(String username, String password, String nickname);
+    User create(String username, String password);
 
     User find(UUID userId);
 
     List<User> findAll();
 
-    User update(UUID userId, String username, String password, String nickname);
+    User update(UUID userId, String username, String password);
 
     void delete(UUID userId);
 }
