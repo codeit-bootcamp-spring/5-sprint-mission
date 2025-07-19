581eb511784e18bdd8661fc3443e582371e4f5e2
BakJiSeok <commicat2@gmail.com>
2025-07-20 04:42:00 +0900

    refactor: add null check for senderId and receiverId in FriendRequest constructor

--- src/main/java/com/sprint/mission/discodeit/entity/FriendRequest.java
+++ src/main/java/com/sprint/mission/discodeit/entity/FriendRequest.java
@@ -7,6 +7,9 @@ public class FriendRequest extends AbstractBaseEntity {
   private final UUID receiverId;
 
   public FriendRequest(UUID senderId, UUID receiverId) {
+    if (senderId == null || receiverId == null) {
+      throw new IllegalArgumentException("SenderId와 ReceiverId는 null일 수 없습니다.");
+    }
     this.senderId = senderId;
     this.receiverId = receiverId;
   }
