6d8eab73d81f4bc6843c7a1d6fad859535e5abdb
DONGMIN-777 <ehdaksl96@naver.com>
2025-07-20 23:02:27 +0900

    feat: modify user.java

---
+++ src/main/java/com/sprint/mission/discodeit/service/User.java
@@ -0,0 +1,18 @@
+package com.sprint.mission.discodeit.service;
+
+public class User {
+    private Long id;
+    private String name;
+
+    public User(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() { return id; }
+    public String getName() { return name; }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
