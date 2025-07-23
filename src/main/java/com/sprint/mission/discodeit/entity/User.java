dda826f55f20d77a0cfa5719e0317c582dd4500a
backKim1024 <dydgml7313@naver.com>
2025-07-23 16:59:42 +0900

    feat(User) : Field Extensions and Modifying Generators
    기존 nickName과 id만으로는 사용자 식별 정확도가 낮아, email, password 등의 필드를 추가하였고, 이에 따라 더미 데이터 생성 로직도 수정함.

--- src/main/java/com/sprint/mission/discodeit/entity/User.java
+++ src/main/java/com/sprint/mission/discodeit/entity/User.java
@@ -1,23 +1,33 @@
 package com.sprint.mission.discodeit.entity;
 
+import java.io.Serializable;
 import java.util.UUID;
 
-public class User {
+public class User implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    // 1. 필드 선언
     private final UUID id;
-    private String nickName;
     private final Long createdAt;
     private Long updatedAt;
 
+    private String name;
+    private String email;
+    private String password;
 
-    public User(String nickName) {
+    // 2. 생성자
+    public User(String name, String email, String password) {
         this.id = UUID.randomUUID();
         this.createdAt = System.currentTimeMillis();
-        this.updatedAt = this.createdAt;
-        this.nickName = nickName;
+        this.updatedAt = null;
+
+        this.name = name;
+        this.email = email;
+        this.password = password;
     }
 
     public void updateNickName(String nickName){
-        this.nickName = nickName;
+        this.name = nickName;
         this.updatedAt = System.currentTimeMillis();
     }
 
@@ -25,8 +35,8 @@ public class User {
         return id;
     }
 
-    public String getNickName() {
-        return nickName;
+    public String getName() {
+        return name;
     }
 
     public Long getUpdatedAt() {
@@ -37,7 +47,7 @@ public class User {
     public String toString() {
         final StringBuilder sb = new StringBuilder("User{");
         sb.append("id=").append(id);
-        sb.append(", nickName='").append(nickName).append('\'');
+        sb.append(", nickName='").append(name).append('\'');
         sb.append(", createdAt=").append(createdAt);
         sb.append(", updatedAt=").append(updatedAt);
         sb.append('}');
