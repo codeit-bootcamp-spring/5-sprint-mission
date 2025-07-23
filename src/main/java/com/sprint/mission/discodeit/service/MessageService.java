48778c255fd08794af37bc2ef35704f8894c2549
polodumbo <polosh1208@gmail.com>
2025-07-23 16:23:11 +0900

    feat: add Message.java, MessageService.java, JCFMessageService.java

---
+++ src/main/java/com/sprint/mission/discodeit/service/MessageService.java
@@ -0,0 +1,23 @@
+package com.sprint.mission.discodeit.service;
+
+import com.sprint.mission.discodeit.entity.Message;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface MessageService {
+    // 메시지 추가
+    Message insert(String content, UUID userId, UUID channelId);
+
+    // 메시지 조회
+    Message selectOne(UUID messageId);
+
+    // 메시지 전체 조회
+    List<Message> selectAll();
+
+    // 메시지 수정
+    Message update(UUID messageId, String content);
+
+    // 메시지 삭제
+    boolean delete(UUID messageId);
+}
