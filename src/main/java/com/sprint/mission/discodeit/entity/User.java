be3c06fee7235baf38b2195c67ece2b8a7264ac8
backKim1024 <dydgml7313@naver.com>
2025-07-23 17:10:05 +0900

    feat(User) : Field Extensions and Modifying Generators
    기존 nickName과 id만으로는 사용자 식별 정확도가 낮아, email, password 등의 필드를 추가하였고, 이에 따라 더미 데이터 생성 로직도 수정함.

--- src/main/java/com/sprint/mission/discodeit/entity/User.java
+++ src/main/java/com/sprint/mission/discodeit/entity/User.java
@@ -24,6 +24,7 @@ public class User implements Serializable {
         this.name = name;
         this.email = email;
         this.password = password;
+
     }
 
     public void updateNickName(String nickName){
