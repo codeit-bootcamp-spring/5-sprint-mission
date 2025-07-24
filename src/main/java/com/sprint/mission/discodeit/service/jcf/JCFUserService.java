abcb0d3d8254208bc83523afbb0037dac6a59b55
Soyeon <isylsy166@gmail.com>
2025-07-24 14:30:18 +0900

    refactor: JCFUserService 데이터 접근 로직을 Repository로 위임

--- src/main/java/com/sprint/mission/discodeit/service/jcf/JCFUserService.java
+++ src/main/java/com/sprint/mission/discodeit/service/jcf/JCFUserService.java
@@ -1,44 +1,46 @@
 package com.sprint.mission.discodeit.service.jcf;
 
 import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.respository.UserRepository;
 import com.sprint.mission.discodeit.service.UserService;
-
 import java.util.*;
 import java.util.UUID;
 
 public class JCFUserService implements UserService {
 
-    private final Map<UUID, User> data = new HashMap<>();
+    private final UserRepository userRepository;
+
+    public JCFUserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     // 생성
     public User create(String name, String password) {
         User user = new User(name, password);
-        data.put(user.getId(), user);
-        return user;
+        return userRepository.save(user);
     }
 
     //조회
     public Optional<User> findById(UUID id) {
-        return Optional.ofNullable(data.get(id));
+        return Optional.ofNullable(userRepository.findById(id));
     }
 
     public List<User> findAll() {
-        return new ArrayList<>(data.values());
+        return userRepository.findAll();
     }
 
     // 수정
     public User update(UUID id, String name) {
-        User user = data.get(id);
+        User user = userRepository.findById(id);
         if (user == null) {
             throw new NoSuchElementException("해당 ID의 사용자를 찾을 수 없습니다");
         }
         user.updateName(name);
-        return user;
+        return userRepository.save(user); // 변경된 사용자 저장
     }
 
     // 삭제
     public void delete(UUID id) {
-        boolean deleted = data.remove(id) != null;
-        System.out.println(deleted ? "삭제완료!" : "삭제실패: 사용자를 찾을 수 없습니다");
+        userRepository.delete(id);
     }
 }
