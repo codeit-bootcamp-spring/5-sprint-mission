96b5c78edcbca3e38bece757ffd2e25a61229859
Soyeon <isylsy166@gmail.com>
2025-07-24 14:30:53 +0900

    refactor: JCFChannelService 데이터 접근 로직을 Repository로 위임

--- src/main/java/com/sprint/mission/discodeit/service/jcf/JCFChannelService.java
+++ src/main/java/com/sprint/mission/discodeit/service/jcf/JCFChannelService.java
@@ -1,59 +1,62 @@
 package com.sprint.mission.discodeit.service.jcf;
 
 import com.sprint.mission.discodeit.entity.Channel;
+import com.sprint.mission.discodeit.respository.ChannelRepository;
 import com.sprint.mission.discodeit.service.ChannelService;
-
 import java.util.*;
 
 public class JCFChannelService implements ChannelService {
 
-    private final Map<UUID, Channel> data = new HashMap<>();
+    private final ChannelRepository channelRepository;
+
+    public JCFChannelService(ChannelRepository channelRepository) {
+        this.channelRepository = channelRepository;
+    }
 
     // 생성
     public Channel create(String name, String topic){
         Channel channel = new Channel(name, topic);
-        data.put(channel.getId(), channel);
-        return channel;
+        return channelRepository.save(channel);
     }
 
     // 아이디로 조회
     public Optional<Channel> findById(UUID id){
-        return Optional.ofNullable(data.get(id));
+        return Optional.ofNullable(channelRepository.findById(id));
     }
 
     // 이름으로 조회
     public List<Channel> findByName(String name){
-        if (name == null) return Collections.emptyList();
-        return data.values().stream().filter(c -> c.getName().contains(name)).toList();
+        return channelRepository.findByName(name);
     }
 
     // 모든 채널 조회
     public List<Channel> findAll(){
-        return new ArrayList<>(data.values());
+        return channelRepository.findAll();
     }
 
     // 채널명 업데이트
     public Channel updateName(UUID id, String name){
-        Channel channel = data.get(id);
-        if(channel != null){
-            channel.updateName(name);
+        Channel channel = channelRepository.findById(id);
+        if (channel == null) {
+            throw new NoSuchElementException("해당 ID의 채널을 찾을 수 없습니다");
         }
-        return channel;
+        channel.updateName(name);
+        return channelRepository.save(channel);
     }
 
     // 채널 토픽 업데이트
     public Channel updateTopic(UUID id, String topic){
-        Channel channel = data.get(id);
-        if(channel != null){
-            channel.updateTopic(topic);
+        Channel channel = channelRepository.findById(id);
+        if (channel == null) {
+            throw new NoSuchElementException("해당 ID의 토픽을 찾을 수 없습니다");
         }
-        return channel;
+        channel.updateTopic(topic);
+        return channelRepository.save(channel);
     }
 
     // 채널 삭제
     public void deleteById(UUID id){
-        boolean deleted = data.remove(id) != null;
-        System.out.println(deleted ? "삭제완료!" : "삭제실패: 사용자를 찾을 수 없습니다");
+        channelRepository.deleteById(id);
     }
 
 }
