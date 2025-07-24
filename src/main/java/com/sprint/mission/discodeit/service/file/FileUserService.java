b842bd563facb1154cbd35c5cd9f2c1cb475293a
Soyeon <isylsy166@gmail.com>
2025-07-24 13:38:02 +0900

    refactor: FileUserService에 update, delete 메서드 저장 책임을 Repository로 위임

--- src/main/java/com/sprint/mission/discodeit/service/file/FileUserService.java
+++ src/main/java/com/sprint/mission/discodeit/service/file/FileUserService.java
@@ -1,82 +1,53 @@
 package com.sprint.mission.discodeit.service.file;
 
 import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.respository.UserRepository;
 import com.sprint.mission.discodeit.service.UserService;
-
 import java.io.*;
 import java.util.*;
 
 public class FileUserService implements UserService {
 
-    private final String FILE_PATH = "data/user.store";
-    private Map<UUID, User> userMap = new HashMap<>();
+    private final UserRepository userRepository;
 
-    public FileUserService() {
-        loadFromFile();
+    public FileUserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
     }
 
 
     @Override
     public User create(String name, String password) {
+
+        //중복검사
+        List<User> users = userRepository.findAll();
+        for (User user : users) {
+            if (user.getName().equals(name)) {
+                throw new IllegalArgumentException("이미 사용중인 이름입니다.");
+            }
+        }
+
         User user = new User(name, password);
-        userMap.put(user.getId(), user);
-        saveToFile();
-        return user;
+        return userRepository.save(user);
     }
 
     @Override
     public Optional<User> findById(UUID id) {
-        return Optional.ofNullable(userMap.get(id));
+        return Optional.ofNullable(userRepository.findById(id));
     }
 
     @Override
     public List<User> findAll() {
-        return new ArrayList<>(userMap.values());
+        return userRepository.findAll();
     }
 
     @Override
     public User update(UUID id, String name) {
-        User user = userMap.get(id);
-        if (user != null) {
-            user.updateName(name);
-            saveToFile();
-        }
-        return user;
+        return userRepository.update(id, name);
     }
 
     @Override
     public void delete(UUID id) {
-        userMap.remove(id);
-        saveToFile();
+        userRepository.delete(id);
     }
-
-    private void saveToFile() {
-        File file = new File(FILE_PATH);
-        file.getParentFile().mkdirs(); // 디렉토리 없으면 생성
-
-        // 직렬화 방식
-        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(file))) {
-            oos.writeObject(userMap);
-        } catch (IOException e) {
-            throw new RuntimeException("사용자 저장 중 오류 발생", e);
-        }
-    }
-
-    @SuppressWarnings("unchecked")
-    private void loadFromFile() {
-        File file = new File(FILE_PATH);
-        if (!file.exists()) return;
-
-        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(file))) {
-            Object obj = ois.readObject();
-            if (obj instanceof Map) {
-                userMap = (Map<UUID, User>) obj;
-            }
-        } catch (IOException | ClassNotFoundException e) {
-            System.out.println("사용자 불러오기 실패, 빈 상태로 초기화");
-            userMap = new HashMap<>();
-        }
-    }
-
 
 }
