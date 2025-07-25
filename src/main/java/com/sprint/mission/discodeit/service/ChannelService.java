98abb84b043a7df756daa9d6deb1ef470434b560
truuuely <kmyc7030@gmail.com>
2025-07-25 23:13:43 +0900

    feat: UserService, ChannelService, MessageService 인터페이스 정의

---
+++ src/main/java/com/sprint/mission/discodeit/service/ChannelService.java
@@ -0,0 +1,18 @@
+package com.sprint.mission.discodeit.service;
+
+import com.sprint.mission.discodeit.entity.Channel;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface ChannelService {
+    Channel save(Channel channel);
+
+    Channel findById(UUID id);
+
+    List<Channel> findAll();
+
+    Channel update(UUID id, Channel channelDto);
+
+    void delete(UUID id);
+}
