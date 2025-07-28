ea7862eb8b542d9db46c5c685c9e65db44133545
kimyumin03 <alsfla03@gmail.com>
2025-07-28 09:46:55 +0900

    sprint mission2: p1 Message Exception handling

--- src/main/java/com/sprint/mission/discodeit/service/jcf/JCFMessageService.java
+++ src/main/java/com/sprint/mission/discodeit/service/jcf/JCFMessageService.java
@@ -1,15 +1,25 @@
 package com.sprint.mission.discodeit.service.jcf;
 
 import com.sprint.mission.discodeit.entity.Message;
+import com.sprint.mission.discodeit.repository.UserRepository;
 import com.sprint.mission.discodeit.service.MessageService;
 
 import java.util.*;
 
 public class JCFMessageService implements MessageService {
     private final Map<UUID, Message> data = new HashMap<>();
+    private final UserRepository userRepository;
+
+    public JCFMessageService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @Override
     public Message create(Message message) {
+        if (userRepository.findById(message.getUserId()) == null) {
+            throw new IllegalArgumentException("존재하지 않는 유저입니다: " + message.getUserId());
+        }
+
         data.put(message.getId(), message);
         return message;
     }
@@ -37,3 +47,5 @@ public class JCFMessageService implements MessageService {
         data.remove(id);
     }
 }
+
+
