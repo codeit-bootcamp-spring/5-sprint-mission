1b0710ed738c8014445ff0091d7a759c557d2128
JJong2001 <diekfk1@naver.com>
2025-07-28 12:18:54 +0900

    feat: (User, Message, Channel) Repository 인터페이스 정의

---
+++ src/main/java/com/sprint/mission/discodeit/repository/ChannelRepository.java
@@ -0,0 +1,55 @@
+package com.sprint.mission.discodeit.repository;
+
+import com.sprint.mission.discodeit.entity.Channel;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface ChannelRepository {
+    /**
+     * 채널 객체를 저장합니다.
+     *
+     * @param channel 저장할 채널 객체
+     */
+    void save(Channel channel);
+
+    /**
+     * ID(UUID)를 기준으로 채널을 조회합니다.
+     *
+     * @param id 조회할 채널의 UUID
+     * @return 해당 ID의 채널 객체, 없으면 null
+     */
+    Channel findById(UUID id);
+
+    /**
+     * 채널 이름으로 채널 목록을 조회합니다. (부분 일치 포함 가능)
+     *
+     * @param name 검색할 채널 이름
+     * @return 이름이 일치하는 채널 리스트
+     */
+    List<Channel> findByName(String channelName);
+
+    /**
+     * 저장된 모든 채널 목록을 반환합니다.
+     *
+     * @return 전체 채널 리스트
+     */
+    List<Channel> findAll();
+
+    /**
+     * 주어진 ID를 가진 채널을 수정합니다.
+     *
+     * @param id 수정 대상 채널 ID
+     * @param updatedChannel 수정된 채널 객체
+     * @return 수정 성공 여부
+     */
+    boolean updateById(UUID id, Channel updatedChannel);
+
+    /**
+     * 주어진 ID를 가진 채널을 삭제합니다.
+     *
+     * @param id 삭제 대상 채널 ID
+     * @return 삭제 성공 여부
+     */
+    boolean deleteById(UUID id);
+}
---
+++ src/main/java/com/sprint/mission/discodeit/repository/MessageRepository.java
@@ -0,0 +1,49 @@
+package com.sprint.mission.discodeit.repository;
+
+import com.sprint.mission.discodeit.entity.Message;
+import com.sprint.mission.discodeit.entity.User;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface MessageRepository {
+
+    /**
+     * 메시지를 저장합니다.
+     * @param message 저장할 메시지 객체
+     */
+    void save(Message message);
+
+    /**
+     * 특정 사용자가 작성한 메시지를 조회합니다.
+     * @param user 조회할 사용자
+     * @return 해당 사용자의 메시지 목록
+     */
+    List<Message> findByUser(User user);
+
+    /**
+     * 메시지 내용으로 메시지를 검색합니다. (부분 일치 가능)
+     * @param message 검색할 메시지 내용
+     * @return 해당 내용을 포함한 메시지 목록
+     */
+    List<Message> findByMessage(String message);
+
+    /**
+     * 모든 메시지를 조회합니다.
+     * @return 메시지 목록
+     */
+    List<Message> findAll();
+
+    /**
+     * 메시지를 수정합니다.
+     * @param id 수정할 메세지의 ID
+     * @param message 수정할 메시지 객체
+     */
+    void update(UUID id, Message message);
+
+    /**
+     * 메시지를 삭제합니다.
+     * @param id 삭제할 메시지의 ID
+     */
+    void delete(UUID id);
+}
