ef525dd15474a3eb2b5234210ba458403319fc37
sooyeonz <5196ksy@naver.com>
2025-07-28 20:22:24 +0900

    feat: add repository domain model interface

---
+++ src/main/java/com/sprint/mission/discodeit/repository/ChannelRepository.java
@@ -0,0 +1,21 @@
+package com.sprint.mission.discodeit.repository;
+
+import com.sprint.mission.discodeit.entity.Channel;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface ChannelRepository {
+    Channel save(Channel channel);
+
+    Optional<Channel> findById(UUID id);
+
+    List<Channel> findAll();
+
+    long count();
+
+    Channel delete(UUID id);
+
+    boolean existById(UUID id);
+}
