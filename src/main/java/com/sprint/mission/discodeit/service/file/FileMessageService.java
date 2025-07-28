15bbd8b4cf009560403aef78ebcc5b5a673de3f3
JJong2001 <diekfk1@naver.com>
2025-07-28 19:07:34 +0900

    feat: File*Service 구현

---
+++ src/main/java/com/sprint/mission/discodeit/service/file/FileMessageService.java
@@ -0,0 +1,58 @@
+package com.sprint.mission.discodeit.service.file;
+
+import com.sprint.mission.discodeit.entity.Channel;
+import com.sprint.mission.discodeit.entity.Message;
+import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.repository.MessageRepository;
+import com.sprint.mission.discodeit.repository.file.FileMessageRepository;
+import com.sprint.mission.discodeit.service.MessageService;
+
+import java.util.List;
+import java.util.UUID;
+
+public class FileMessageService implements MessageService {
+    private final MessageRepository messageRepository;
+
+    public FileMessageService() {
+        messageRepository = new FileMessageRepository();
+    }
+
+    @Override
+    public Message createMessage(User user, Channel channel, String message) {
+        Message m =  new Message(user, channel, message);
+        messageRepository.save(m);
+
+        return m;
+    }
+
+    @Override
+    public List<Message> getByUser(User user) {
+        return messageRepository.findByUser(user);
+    }
+
+    @Override
+    public List<Message> getByMessage(String message) {
+        return messageRepository.findByMessage(message);
+    }
+
+    @Override
+    public boolean updateById(UUID id, User user, Channel channel, String message) {
+        return messageRepository.findAll().stream()
+                .filter(m -> m.getId().equals(id))
+                .filter(m -> m.getUser().equals(user))
+                .filter(m -> m.getChannel().equals(channel))
+                .findFirst()
+                .map(m -> {
+                    Message update = new Message(id, user, channel, message, m.getCreateAt());
+                    messageRepository.update(id, update);
+                    return true;
+                })
+                .orElse(false);
+    }
+
+    @Override
+    public boolean removeById(UUID id, User user, Channel channel) {
+        return messageRepository.findAll()
+                .removeIf(m -> m.getId().equals(id) && m.getUser().equals(user) && m.getChannel().equals(channel));
+    }
+}
