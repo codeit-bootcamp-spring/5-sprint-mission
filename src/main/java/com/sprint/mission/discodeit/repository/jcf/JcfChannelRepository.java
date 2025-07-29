76d977e8dbe94dc139e4e8afc1038ac7ad4b019d
backKim1024 <dydgml7313@naver.com>
2025-07-29 21:14:11 +0900

    feat(channel) : Implement jcf-base ChannelRepository
    - JcfChannelRepository 클래스 추가
    채널 저장 및 조회를 위한 JCF 기반 저장소 구현
    save, findById, findByName, findAll, delete 메서드 포함
    - 로그 출력으로 캐시 동작 확인 기능

---
+++ src/main/java/com/sprint/mission/discodeit/repository/jcf/JcfChannelRepository.java
@@ -0,0 +1,48 @@
+package com.sprint.mission.discodeit.repository.jcf;
+
+import com.sprint.mission.discodeit.entity.Channel;
+import com.sprint.mission.discodeit.repository.ChannelRepository;
+
+import java.util.*;
+
+public class JcfChannelRepository implements ChannelRepository {
+    // 사용자 데이터를 저장할 HashMap. UUID를 키로, Channel 객체를 값으로 사용
+    private final Map<UUID, Channel> channels = new HashMap<>();
+
+
+    @Override
+    public Channel save(Channel channel) {
+        channels.put(channel.getId(), channel);
+        System.out.println("Channel saved to JCF cache: " + channel.getId());
+        return channel;
+    }
+
+    @Override
+    public Optional<Channel> findById(UUID id) {
+        System.out.println("Finding Channel by ID in JCF cache: " + id);
+        return Optional.ofNullable(channels.get(id));
+    }
+
+    @Override
+    public Optional<Channel> findByName(String name) {
+        System.out.println("Finding channel by email in JCF cache: " + name);
+        return channels.values().stream()
+                .filter(channel -> channel.getName() != null && channel.getName().equals(name))
+                .findFirst(); //
+    }
+
+    @Override
+    public List<Channel> findAll() {
+        System.out.println("Retrieving all channel from JCF cache. Total: " + channels.size());
+        return new ArrayList<>(channels.values());
+    }
+
+    @Override
+    public void delete(UUID id) {
+        if(!channels.containsKey(id)) {
+            throw new NoSuchElementException("Channel with id " + id + " not found");
+        }
+        channels.remove(id);
+        System.out.println("Channel deleted from JCF cache: "+ id);
+    }
+}
