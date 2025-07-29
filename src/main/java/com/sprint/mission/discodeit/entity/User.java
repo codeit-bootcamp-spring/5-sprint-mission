48a60a825b9ddf450907e0c26a17d6740b1ca47c
Soyeon <isylsy166@gmail.com>
2025-07-29 18:40:07 +0900

    refactor: User 엔티티 email 필드 추가 및 비밀번호 변경 로직 개선

--- src/main/java/com/sprint/mission/discodeit/entity/User.java
+++ src/main/java/com/sprint/mission/discodeit/entity/User.java
@@ -5,32 +5,55 @@ import java.io.Serializable;
 public class User extends Base implements Serializable {
 
     private String name;
+    private String email;
     private String password;
 
-    public User(String name, String password) {
+    public User(String name, String email, String password) {
         if(name==null || name.isBlank()){ throw new IllegalArgumentException("이름은 null이 될 수 없음");}
+        if(email==null || email.isBlank()){ throw new IllegalArgumentException("이메일은 null이 될 수 없음");}
         if(password==null || password.isBlank()){ throw new IllegalArgumentException("비밀번호는 null이 될 수 없음");}
         this.name = name;
+        this.email = email;
         this.password = password;
     }
 
-    public String getName() {return name;}
+    public String getName() {
+        return name;
+    }
 
     public void updateName(String newName) {
         if(newName==null || newName.isBlank()){ throw new IllegalArgumentException("이름은 null이 될 수 없음");}
         this.name = newName;
         updateTimestamp();
     }
-    public String getPassword() {return password;}
 
-    public void updatePassword(String newPassword) {
-        if(newPassword==null || newPassword.isBlank()){ throw new IllegalArgumentException("비밀번호는 null이 될 수 없음");}
+    public void updatePassword(String currentPassword, String newPassword) {
+        if(!this.password.equals(currentPassword)){
+            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다");
+        }
+        if(newPassword==null || newPassword.isBlank()){
+            throw new IllegalArgumentException("새 비밀번호는 null이거나 공백일 수 없습니다.");
+        }
+        if (newPassword.equals(currentPassword)) {
+            throw new IllegalArgumentException("새 비밀번호는 기존 비밀번호와 달라야 합니다.");
+        }
+        // 추후 hash 처리 로직 추가
         this.password = newPassword;
+        updateTimestamp();
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void updateEmail(String newEmail) {
+        if(newEmail==null || newEmail.isBlank()){ throw new IllegalArgumentException("이메일은 null이 될 수 없음");}
+        this.email = newEmail;
         updateTimestamp();
     }
 
     @Override
     public String toString() {
-        return "\nid: " + getId() + "|  name: " + name;
+        return "id: " + getId() + "|  name: " + name + "|  email: " + email;
     }
 }
