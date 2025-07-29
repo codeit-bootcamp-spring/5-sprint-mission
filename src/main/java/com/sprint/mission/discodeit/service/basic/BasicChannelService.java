9e7d11edba249d8fef0c6e460876d097b6e8ae67
backKim1024 <dydgml7313@naver.com>
2025-07-29 22:23:18 +0900

    feat(channel) : Implement BasicChannelService
    - 비즈니스 로직을 담당하는 BasicChannelService 구현
    채널 생성 후 Repository를 통해 저장
    findById, findByName, findAll은 Repository를 통해 저장된 값을 불러오도록 구현
    이름 변경시 업데이트가 되도록 구현
    채널 유효성 검사 후 해당 채널이 삭제되도록 구현
    - 로그 출력 기능 구현

--- src/main/java/com/sprint/mission/discodeit/service/basic/BasicChannelService.java
+++ src/main/java/com/sprint/mission/discodeit/service/basic/BasicChannelService.java
@@ -1,4 +1,84 @@
 package com.sprint.mission.discodeit.service.basic;
 
-public class BasicChannelService {
+import com.sprint.mission.discodeit.entity.Channel;
+import com.sprint.mission.discodeit.repository.ChannelRepository;
+import com.sprint.mission.discodeit.repository.UserRepository;
+import com.sprint.mission.discodeit.service.ChannelService;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public class BasicChannelService implements ChannelService {
+
+    private final ChannelRepository channelRepository;
+    private final UserRepository userRepository;
+
+    public BasicChannelService(ChannelRepository channelRepository, UserRepository userRepository) {
+        this.channelRepository = channelRepository;
+        this.userRepository = userRepository;
+    }
+
+
+    @Override
+    public Optional<Channel> createChannel(String name) {
+        // 1. 채널 이름 중복 검사
+        Optional<Channel> existingChannel = channelRepository.findByName(name);
+        if (existingChannel.isPresent()) {
+            System.out.println("[Ser]Channel already exists: " + name);
+            return Optional.empty();
+        }
+
+        // 2. 새로운 Channel 객체 생성
+        Channel newChannel = new Channel(name);
+
+        // 3. 새로운 Channel을 Repository에 저장
+        channelRepository.save(newChannel);
+        System.out.println("[Ser]Created channel: " + newChannel);
+        return Optional.of(newChannel);
+    }
+
+    @Override
+    public Optional<Channel> findById(UUID id) {
+        System.out.println("[Ser]Finding channel by id: " + id);
+        return channelRepository.findById(id);
+    }
+
+    @Override
+    public Optional<Channel> findByName(String name) {
+        System.out.println("[Ser]Finding channel by name: " + name);
+        return channelRepository.findByName(name);
+    }
+
+    @Override
+    public List<Channel> findAll() {
+        System.out.println("[Ser]Finding all channels" + channelRepository.findAll().size());
+        return channelRepository.findAll();
+    }
+
+    @Override
+    public void updatedName(UUID id, String name) {
+        try {
+            if (channelRepository.findByName(name).isPresent()) {
+                System.out.println("[Ser]Channel already exists: " + name);
+            } else {
+                Channel channel = channelRepository.findByName(name).get();
+                channel.updateName(name);
+                channelRepository.save(new Channel(name));
+                System.out.println("[Ser]Updated channel: " + channel);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void delete(UUID id) {
+        if (channelRepository.findById(id).isPresent()) {
+            System.out.println("[Ser]Channel delete: " + id);
+            channelRepository.delete(id);
+        } else {
+            System.out.println("[Ser]Channel not found: " + id);
+        }
+    }
 }
