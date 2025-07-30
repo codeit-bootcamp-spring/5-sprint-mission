e0806b056779d2135d95876382a2fc22eda80c50
yelim-Lee <dpfla1078@naver.com>
2025-07-30 18:38:15 +0900

    유저 심화사항

---
+++ src/main/java/com/sprint/mission/discodeit/service/basic/BasicUserService.java
@@ -0,0 +1,50 @@
+package com.sprint.mission.discodeit.service.basic;
+
+import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.repository.UserRepository;
+import com.sprint.mission.discodeit.service.UserService;
+
+import java.util.*;
+
+public class BasicUserService implements UserService {
+    private final UserRepository repo;
+
+    public BasicUserService(UserRepository repo) {
+        this.repo = repo;
+    }
+
+    @Override
+    public User createUser(String username) {
+        User user = new User(username);
+       return repo.save(user);
+    }
+
+    @Override
+    public Optional<User> getUser(UUID userId) {
+        Optional<User> user = repo.findById(userId);
+        if(user.isEmpty()){
+            throw new NoSuchElementException("User with id " + userId + " not found");
+        }
+        return user;
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return repo.findAll();
+    }
+
+    @Override
+    public User updateUser(UUID userId, User user) {
+       return repo.update(userId,user);
+    }
+
+    @Override
+    public void deleteUser(UUID userId) {
+        repo.delete(userId);
+    }
+
+    @Override
+    public boolean existsById(UUID id) {
+        return repo.existsById(id);
+    }
+}
