5769ab927f2f5387223e7587f3a0bdf0bbccf060
sooyeonz <5196ksy@naver.com>
2025-07-30 14:01:15 +0900

    feat: 스프린트 미션1 모범코드 반영 - 서비스 간 의존성 주입

--- src/main/java/com/sprint/mission/discodeit/service/jcf/JCFMessageService.java
+++ src/main/java/com/sprint/mission/discodeit/service/jcf/JCFMessageService.java
@@ -1,21 +1,35 @@
 package com.sprint.mission.discodeit.service.jcf;
 
 import com.sprint.mission.discodeit.entity.Message;
+import com.sprint.mission.discodeit.service.ChannelService;
 import com.sprint.mission.discodeit.service.MessageService;
+import com.sprint.mission.discodeit.service.UserService;
 
 import java.util.*;
 
 public class JCFMessageService implements MessageService {
     private final Map<UUID, Message> data;
 
-    public JCFMessageService() {
+    private final ChannelService channelService;
+    private final UserService userService;
+
+    public JCFMessageService(ChannelService channelService, UserService userService) {
         data = new HashMap<>();
+        this.channelService = channelService;
+        this.userService = userService;
     }
 
     @Override
     public Message create(String content, UUID channelId, UUID authorId) {
         if (content == null || content.isBlank() || channelId == null || authorId == null) {
             throw new IllegalArgumentException("message content or channelId or authorId is null or blank");
+        }
+
+        try {
+            channelService.find(channelId);
+            userService.find(authorId);
+        } catch (NoSuchElementException e) {
+            throw e;
         }
 
         Message message = new Message(content, channelId, authorId);
