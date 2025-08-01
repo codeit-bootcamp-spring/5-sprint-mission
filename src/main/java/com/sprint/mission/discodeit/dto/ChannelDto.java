e2758f31820f0e103b32a45a3774c66bf65a54ce
Soyeon <isylsy166@gmail.com>
2025-08-01 17:34:53 +0900

    feat: Message, Channel에 Create DTO 추가

--- src/main/java/com/sprint/mission/discodeit/dto/ChannelDto.java
+++ src/main/java/com/sprint/mission/discodeit/dto/ChannelDto.java
@@ -1,4 +1,14 @@
 package com.sprint.mission.discodeit.dto;
 
+import com.sprint.mission.discodeit.entity.ChannelType;
+import org.springframework.lang.Nullable;
+
 public class ChannelDto {
+
+    public record Create(
+        String name,
+        ChannelType type,
+        @Nullable String topic,
+        @Nullable String description
+    ) {}
 }
--- src/main/java/com/sprint/mission/discodeit/dto/MessageDto.java
+++ src/main/java/com/sprint/mission/discodeit/dto/MessageDto.java
@@ -1,4 +1,14 @@
 package com.sprint.mission.discodeit.dto;
 
+import com.sprint.mission.discodeit.entity.Channel;
+import com.sprint.mission.discodeit.entity.User;
+import org.springframework.lang.Nullable;
+
 public class MessageDto {
+
+    public record Create(
+            User user,
+            Channel channel,
+            String content
+    ) {}
 }
