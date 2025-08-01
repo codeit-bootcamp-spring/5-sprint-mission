3ca923937bf01ecfb0394765759aa9031b005c74
yelim-Lee <dpfla1078@naver.com>
2025-08-01 16:13:36 +0900

    feat: FileChannelService.java

--- src/main/java/com/sprint/mission/discodeit/service/file/FileChannelService.java
+++ src/main/java/com/sprint/mission/discodeit/service/file/FileChannelService.java
@@ -1,4 +1,43 @@
 package com.sprint.mission.discodeit.service.file;
 
-public class FileChannelService {
+import com.sprint.mission.discodeit.entity.Channel;
+import com.sprint.mission.discodeit.repository.file.FileChannelRepository;
+import com.sprint.mission.discodeit.service.ChannelService;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public class FileChannelService implements ChannelService {
+
+    private final FileChannelRepository fileRepo;
+
+    public FileChannelService(FileChannelRepository fileRepo) {
+        this.fileRepo = fileRepo;
+    }
+
+    @Override
+    public Channel createChannel(String channelname) {
+        return fileRepo.save(new Channel(channelname));
+    }
+
+    @Override
+    public Optional<Channel> getChannel(UUID channelId) {
+        return fileRepo.findById(channelId);
+    }
+
+    @Override
+    public List<Channel> getAllChannels() {
+        return fileRepo.findAll();
+    }
+
+    @Override
+    public Channel updateChannel(UUID channelId, String channelname) {
+        return fileRepo.update(channelId, channelname);
+    }
+
+    @Override
+    public void deleteChannel(UUID channelId) {
+        fileRepo.delete(channelId);
+    }
 }
