2796a489aed3b36aa2aab9c07b0b11bf43cacd54
JaehyeokLim <217686834+JaehyeokLim@users.noreply.github.com>
2025-08-02 21:49:24 +0900

    ✨JCF*Repository 구현, 저장 로직 분리

--- src/main/java/com/sprint/mission/discodeit/repository/jcf/JCFUserRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/jcf/JCFUserRepository.java
@@ -1,4 +1,36 @@
 package com.sprint.mission.discodeit.repository.jcf;
 
-public class JCFUserRepository {
+import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.repository.UserRepository;
+
+import java.util.*;
+
+public class JCFUserRepository implements UserRepository {
+
+    private final Map<UUID, User> data;
+
+    public JCFUserRepository() {
+        this.data = new HashMap<>();
+    }
+
+    @Override
+    public User save(User user) {
+        data.put(user.getId(), user);
+        return user;
+    }
+
+    @Override
+    public Optional<User> findById(UUID id) {
+        return Optional.ofNullable(data.get(id));
+    }
+
+    @Override
+    public List<User> findAll() {
+        return data.values().stream().toList();
+    }
+
+    @Override
+    public void deleteById(UUID id) {
+        data.remove(id);
+    }
 }
