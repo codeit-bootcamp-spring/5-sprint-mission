773f8e981e325d85e0868d7bf3d3a725592c2580
JaehyeokLim <217686834+JaehyeokLim@users.noreply.github.com>
2025-08-02 21:49:13 +0900

    ✨File*Repository 구현, 저장 로직 분리

--- src/main/java/com/sprint/mission/discodeit/repository/file/FileMessageRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/file/FileMessageRepository.java
@@ -1,4 +1,96 @@
 package com.sprint.mission.discodeit.repository.file;
 
-public class FileMessageRepository {
+import com.sprint.mission.discodeit.entity.Message;
+import com.sprint.mission.discodeit.repository.MessageRepository;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.stream.Stream;
+
+public class FileMessageRepository implements MessageRepository {
+
+    private final Path DIRECTORY_PATH = Paths.get("./data/messages");
+
+    public FileMessageRepository() {
+        if (Files.notExists(DIRECTORY_PATH)) {
+            try {
+                Files.createDirectories(DIRECTORY_PATH);
+            } catch (IOException e) {
+                throw new RuntimeException("Failed to create directory: " + DIRECTORY_PATH, e);
+            }
+        }
+    }
+
+    private Path getMessageFilePath(UUID id) {
+        return DIRECTORY_PATH.resolve(id + ".ser");
+    }
+
+    @Override
+    public Message save(Message message) {
+        try(FileOutputStream fos = new FileOutputStream(getMessageFilePath(message.getId()).toFile());
+            ObjectOutputStream oos = new ObjectOutputStream(fos)
+        ) {
+            oos.writeObject(message);
+
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to create message: " + message, e);
+        }
+
+        return message;
+    }
+
+    @Override
+    public Optional<Message> findById(UUID id) {
+        Message message;
+
+        try (FileInputStream fis = new FileInputStream(getMessageFilePath(id).toFile());
+             ObjectInputStream ois = new ObjectInputStream(fis)
+        ) {
+
+            message = (Message) ois.readObject();
+
+        } catch (NoSuchElementException e) {
+            throw new NoSuchElementException("Message with id " + id + " not found");
+        } catch (IOException | ClassNotFoundException e) {
+            throw new RuntimeException("Failed to read message: " + id, e);
+        }
+
+        return Optional.ofNullable(message);
+    }
+
+    @Override
+    public List<Message> findAll() {
+        try (Stream<Path> pathStream = Files.list(DIRECTORY_PATH)) {
+            return pathStream
+                    .map(path -> {
+                        try (FileInputStream fis = new FileInputStream(path.toFile());
+                             ObjectInputStream ois = new ObjectInputStream(fis)
+                        ) {
+                            Object data = ois.readObject();
+                            return (Message) data;
+                        } catch (IOException | ClassNotFoundException e) {
+                            throw new RuntimeException(e);
+                        }
+                    })
+                    .toList();
+
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to read message", e);
+        }
+    }
+
+    @Override
+    public void deleteById(UUID id) {
+        try {
+            Files.delete(getMessageFilePath(id));
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to read message: " + id, e);
+        }
+    }
 }
--- src/main/java/com/sprint/mission/discodeit/repository/file/FileUserRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/file/FileUserRepository.java
@@ -1,4 +1,96 @@
 package com.sprint.mission.discodeit.repository.file;
 
-public class FileUserRepository {
+import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.repository.UserRepository;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.stream.Stream;
+
+public class FileUserRepository implements UserRepository {
+
+    private final Path DIRECTORY_PATH = Paths.get("./data/users");
+
+    public FileUserRepository() {
+        if (Files.notExists(DIRECTORY_PATH)) {
+            try {
+                Files.createDirectories(DIRECTORY_PATH);
+            } catch (IOException e) {
+                throw new RuntimeException("Failed to create directory: " + DIRECTORY_PATH, e);
+            }
+        }
+    }
+
+    private Path getUserFilePath(UUID id) {
+        return DIRECTORY_PATH.resolve(id + ".ser");
+    }
+
+    @Override
+    public User save(User user) {
+        try(FileOutputStream fos = new FileOutputStream(getUserFilePath(user.getId()).toFile());
+            ObjectOutputStream oos = new ObjectOutputStream(fos)
+        ) {
+            oos.writeObject(user);
+
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to create user: " + user, e);
+        }
+
+        return user;
+    }
+
+    @Override
+    public Optional<User> findById(UUID id) {
+        User user;
+
+        try (FileInputStream fis = new FileInputStream(getUserFilePath(id).toFile());
+             ObjectInputStream ois = new ObjectInputStream(fis)
+        ) {
+
+            user = (User) ois.readObject();
+
+        } catch (NoSuchElementException e) {
+            throw new NoSuchElementException("User with id " + id + " not found");
+        } catch (IOException | ClassNotFoundException e) {
+            throw new RuntimeException("Failed to read user: " + id, e);
+        }
+
+        return Optional.ofNullable(user);
+    }
+
+    @Override
+    public List<User> findAll() {
+        try (Stream<Path> pathStream = Files.list(DIRECTORY_PATH)) {
+            return pathStream
+                    .map(path -> {
+                        try (FileInputStream fis = new FileInputStream(path.toFile());
+                             ObjectInputStream ois = new ObjectInputStream(fis)
+                        ) {
+                            Object data = ois.readObject();
+                            return (User) data;
+                        } catch (IOException | ClassNotFoundException e) {
+                            throw new RuntimeException(e);
+                        }
+                    })
+                    .toList();
+
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to read users", e);
+        }
+    }
+
+    @Override
+    public void deleteById(UUID id) {
+        try {
+            Files.delete(getUserFilePath(id));
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to read user: " + id, e);
+        }
+    }
 }
