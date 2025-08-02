acd84ba59f17795e984d5079c82cbba686d6ed83
JaehyeokLim <217686834+JaehyeokLim@users.noreply.github.com>
2025-08-02 16:48:53 +0900

    ✨File*Service Test 추가

--- src/main/java/com/sprint/mission/discodeit/JavaApplication.java
+++ src/main/java/com/sprint/mission/discodeit/JavaApplication.java
@@ -7,8 +7,10 @@ import com.sprint.mission.discodeit.entity.enums.ChannelType;
 import com.sprint.mission.discodeit.service.ChannelService;
 import com.sprint.mission.discodeit.service.MessageService;
 import com.sprint.mission.discodeit.service.UserService;
+import com.sprint.mission.discodeit.service.file.FileChannelService;
+import com.sprint.mission.discodeit.service.file.FileMessageService;
+import com.sprint.mission.discodeit.service.file.FileUserService;
 import com.sprint.mission.discodeit.service.jcf.JCFChannelService;
-import com.sprint.mission.discodeit.service.jcf.JCFUserService;
 import com.sprint.mission.discodeit.service.jcf.JCFMessageService;
 
 import java.util.List;
@@ -17,38 +19,50 @@ import static com.sprint.mission.discodeit.entity.enums.ChannelType.*;
 
 public class JavaApplication {
 
-    static void userCRUDTest(UserService userService) {
+    static void userCRUDTest(UserService userService) throws InterruptedException {
         // 생성
-        User user = userService.create("woody", "woody@codeit.com", "woody1234");
-        System.out.println("유저 생성: " + user.getId());
+        User user1 = userService.create("jae", "jae@example.com", "jae1234");
+        User user2 = userService.create("hyeok", "hyeok@example.com", "hyeok1234");
+        System.out.println("유저 생성: " + user1);
+        System.out.println("유저 생성: " + user2);
+
         // 조회
-        User foundUser = userService.findById(user.getId());
-        System.out.println("유저 조회(단건): " + foundUser.getId());
-        List<User> foundUsers = userService.findAll();
-        System.out.println("유저 조회(다건): " + foundUsers.size());
+        System.out.println("유저 조회(단건): " + userService.findById(user1.getId()));
+        System.out.println("유저 조회(다건): " + userService.findAll().size());
+        System.out.println("유저 조회(다건): " + userService.findAll());
+
         // 수정
-        User updatedUser = userService.update(user.getId(), null, null, "woody5678");
+        User updatedUser = userService.update(user1.getId(), null, null, "jae4321");
         System.out.println("유저 수정: " + String.join("/", updatedUser.getName(), updatedUser.getEmail(), updatedUser.getPassword()));
+        System.out.println("유저 조회(단건): " + userService.findById(user1.getId()));
+
         // 삭제
-        userService.delete(user.getId());
+        userService.delete(user1.getId());
+        userService.delete(user2.getId());
         List<User> foundUsersAfterDelete = userService.findAll();
         System.out.println("유저 삭제: " + foundUsersAfterDelete.size());
     }
 
     static void channelCRUDTest(ChannelService channelService) {
         // 생성
-        Channel channel = channelService.create("공지 채널입니다.", "공지", PUBLIC);
-        System.out.println("채널 생성: " + channel.getId());
+        Channel channel1 = channelService.create("공지 채널입니다.", "공지", PUBLIC);
+        Channel channel2 = channelService.create("스터디 채널입니다.", "스터디", PUBLIC);
+        System.out.println("채널 생성: " + channel1.getId());
+        System.out.println("채널 생성: " + channel2.getId());
+
         // 조회
-        Channel foundChannel = channelService.findById(channel.getId());
-        System.out.println("채널 조회(단건): " + foundChannel.getId());
-        List<Channel> foundChannels = channelService.findAll();
-        System.out.println("채널 조회(다건): " + foundChannels.size());
+        System.out.println("채널 조회(단건): " + channelService.findById(channel1.getId()));
+        System.out.println("채널 조회(다건): " + channelService.findAll().size());
+        System.out.println("채널 조회(다건): " + channelService.findAll());
+
         // 수정
-        Channel updatedChannel = channelService.update(channel.getId(), "공지사항", null, PUBLIC);
+        Channel updatedChannel = channelService.update(channel1.getId(), "공지사항", null, PRIVATE);
         System.out.println("채널 수정: " + String.join("/", updatedChannel.getName(), updatedChannel.getDescription()));
+        System.out.println("채널 조회(단건): " + channelService.findById(channel1.getId()));
+
         // 삭제
-        channelService.delete(channel.getId());
+        channelService.delete(channel1.getId());
+        channelService.delete(channel2.getId());
         List<Channel> foundChannelsAfterDelete = channelService.findAll();
         System.out.println("채널 삭제: " + foundChannelsAfterDelete.size());
     }
@@ -57,28 +71,35 @@ public class JavaApplication {
         // 셋업
         User user = userService.create("woody", "woody@codeit.com", "woody1234");
         Channel channel = channelService.create("공지 채널입니다.", "공지", ChannelType.PUBLIC);
+
         // 생성
-        Message message = messageService.create(user.getId(), channel.getId(), "안녕하세요.");
-        System.out.println("메시지 생성: " + message.getId());
+        Message message1 = messageService.create(user.getId(), channel.getId(), "안녕하세요.");
+        Message message2 = messageService.create(user.getId(), channel.getId(), "안녕하세요.");
+        System.out.println("메시지 생성: " + message1.getId());
+        System.out.println("메시지 생성: " + message2.getId());
+
         // 조회
-        Message foundMessage = messageService.findById(message.getId());
-        System.out.println("메시지 조회(단건): " + foundMessage.getId());
-        List<Message> foundMessages = messageService.findAll();
-        System.out.println("메시지 조회(다건): " + foundMessages.size());
+        System.out.println("메시지 조회(단건): " + messageService.findById(message1.getId()));
+        System.out.println("메시지 조회(다건): " + messageService.findAll().size());
+        System.out.println("메시지 조회(다건): " + messageService.findAll());
+
         // 수정
-        Message updatedMessage = messageService.update(message.getId(), "반갑습니다.");
+        Message updatedMessage = messageService.update(message1.getId(), "반갑습니다.");
         System.out.println("메시지 수정: " + updatedMessage.getContent());
+        System.out.println("메시지 조회(단건): " + messageService.findById(message1.getId()));
+
         // 삭제
-        messageService.delete(message.getId());
+        messageService.delete(message1.getId());
+        messageService.delete(message2.getId());
         List<Message> foundMessagesAfterDelete = messageService.findAll();
         System.out.println("메시지 삭제: " + foundMessagesAfterDelete.size());
     }
 
 
-    public static void main(String[] args) {
-        UserService userService = new JCFUserService();
-        ChannelService channelService = new JCFChannelService();
-        MessageService messageService = new JCFMessageService(channelService, userService);
+    public static void main(String[] args) throws InterruptedException {
+        UserService userService = new FileUserService();
+        ChannelService channelService = new FileChannelService();
+        MessageService messageService = new FileMessageService(channelService, userService);
 
         userCRUDTest(userService);
         channelCRUDTest(channelService);
