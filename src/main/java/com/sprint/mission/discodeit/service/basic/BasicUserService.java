e40daa3cb36d14050e797ec2e8f957b662f2c165
JaehyeokLim <217686834+JaehyeokLim@users.noreply.github.com>
2025-08-02 22:01:25 +0900

    ✨Basic*Service 생성 및 구현
    - 전에 Repository에 데이터 관련 로직을 분리해놓았듯이, Basic*Service에 비즈니스 로직 분리
    - 이후 테스트로 실행 확인
    - 비즈니스, 데이터 관련 로직을 분리함으로써 SRP, ISP 원칙을 지킴

---
+++ src/main/java/com/sprint/mission/discodeit/service/basic/BasicUserService.java
@@ -0,0 +1,56 @@
+package com.sprint.mission.discodeit.service.basic;
+
+import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.repository.UserRepository;
+import com.sprint.mission.discodeit.service.UserService;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+import java.util.UUID;
+
+public class BasicUserService implements UserService {
+
+    private final UserRepository userRepository;
+
+    public BasicUserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public User create(String name, String email, String password) {
+        User user = new User(
+                name,
+                email,
+                password
+        );
+
+        return userRepository.save(user);
+    }
+
+    @Override
+    public User findById(UUID id) {
+        Optional<User> user = userRepository.findById(id);
+        return user.orElseThrow(() -> new NoSuchElementException("User with id " + id + " not found"));
+    }
+
+    @Override
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public User update(UUID id, String name, String email, String password) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new NoSuchElementException("User with id " + id + " not found"));
+        user.update(name, email, password);
+        return userRepository.save(user);
+    }
+
+    @Override
+    public void delete(UUID id) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new NoSuchElementException("User with id " + id + " not found"));
+        userRepository.deleteById(user.getId());
+    }
+}
