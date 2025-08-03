9efc6f83f74b7a26f0e71b14f213cc0b9ae4821d
Haram Jo <harramaram@gmail.com>
2025-08-03 20:10:59 +0900

    refact: repository 생성 및 관련로직 이동 완료

---
+++ src/main/java/com/sprint/mission/discodeit/repository/MessageRepository.java
@@ -0,0 +1,19 @@
+package com.sprint.mission.discodeit.repository;
+
+import com.sprint.mission.discodeit.entity.Message;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface MessageRepository {
+
+    void save(Message message); //생성
+
+    Message findById(UUID id); //하나만 찾기
+
+    List<Message> findAll(); //전체 조회
+
+    void update(Message message); //수정
+
+    void delete(UUID id); //삭제
+}
--- src/main/java/com/sprint/mission/discodeit/service/jcf/JCFMessageService.java
+++ src/main/java/com/sprint/mission/discodeit/service/jcf/JCFMessageService.java
@@ -1,29 +1,24 @@
 package com.sprint.mission.discodeit.service.jcf;
 
 
-import com.sprint.mission.discodeit.entity.Channel;
 import com.sprint.mission.discodeit.entity.Message;
 import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.repository.MessageRepository;
+import com.sprint.mission.discodeit.repository.jcf.JCFMessageRepository;
 import com.sprint.mission.discodeit.service.MessageService;
 import com.sprint.mission.discodeit.service.UserService;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.UUID;
 
 /*실제 구현체*/
 public class JCFMessageService implements MessageService {
 
-    //필드 선언
-    //키:값 구조
-    private final Map<UUID, Message> data;
+    //의존성 주입
+    private final MessageRepository repository = new JCFMessageRepository();
     private final UserService userService; //userService.findById쓰기 위해 주입
 
-    //생성자 주입
-    //data를 빈 HashMap으로 초기화 -> 메세지 담을 공간 만들어둠
     public JCFMessageService(UserService userService) {
-        this.data = new HashMap<>();
         this.userService = userService;
     }
 
@@ -36,38 +31,26 @@ public class JCFMessageService implements MessageService {
         if (sender == null) {
             throw new IllegalArgumentException("보낸 유저가 존재하지 않습니다.");
         }
-        data.put(message.getId(), message); //메세지 객체 받아 map에 uuid-메세지 구조로 저장
+        repository.save(message); //저장은 repository로 위임
     }
 
     @Override
     public Message findById(UUID id) {
-        Message message = data.get(id);
-        if (message == null) {
-            throw new IllegalArgumentException("아이디가 NULL값입니다.");
-        }
-        return new Message(message); // 원본(data.get(id)) 수정 방지용
-
-
+        return repository.findById(id);
     }
 
     @Override
     public List<Message> findAll() {
-        return List.copyOf(data.values()); //copyOf: map에 저장된 키값만 꺼내서 외부에서 수정못하게 막아줌
+        return repository.findAll();
     }
 
     @Override
     public void update(Message message) {
-        if (message == null) {
-            throw new IllegalArgumentException("메세지가 NULL값입니다.");
-        }
-        data.put(message.getId(), message); //같은 uuid면 message 값 덮어씀
+        repository.update(message);
     }
 
     @Override
     public void delete(Message message) {
-        if (!data.containsKey(message.getId())) {
-            throw new IllegalArgumentException("해당 ID를 가진 채널이 존재하지 않습니다.");
-        }
-        data.remove(message.getId());
+        repository.delete(message.getId());
     }
 }
