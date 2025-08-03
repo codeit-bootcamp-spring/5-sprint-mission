da67f1eb8bf8e97aeaf31d84c7510168a0940ba8
yelim-Lee <dpfla1078@naver.com>
2025-08-03 16:19:12 +0900

    createMessage 메서드 매개변수 추가 - UUID channelId - UUID authorId

--- src/main/java/com/sprint/mission/discodeit/service/MessageService.java
+++ src/main/java/com/sprint/mission/discodeit/service/MessageService.java
@@ -5,7 +5,7 @@ import com.sprint.mission.discodeit.entity.Message;
 import java.util.*;
 
 public interface MessageService {
-    Message createMessage(String content);
+    Message createMessage(String content, UUID chanelId, UUID authorId);
     Optional<Message> getMessage(UUID messageId);
     List<Message> getAllMessages();
     Message updateMessage(UUID messageId, String content);
