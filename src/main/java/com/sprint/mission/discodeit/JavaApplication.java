114758126e0d2f4c2aedc82af0a3f3d326d46021
yelim-Lee <dpfla1078@naver.com>
2025-08-03 16:24:07 +0900

    List로 데이터 저장

--- src/main/java/com/sprint/mission/discodeit/JavaApplication.java
+++ src/main/java/com/sprint/mission/discodeit/JavaApplication.java
@@ -7,7 +7,6 @@ import com.sprint.mission.discodeit.repository.file.*;
 import com.sprint.mission.discodeit.repository.jcf.*;
 import com.sprint.mission.discodeit.service.*;
 import com.sprint.mission.discodeit.service.basic.*;
-import com.sprint.mission.discodeit.service.jcf.*;
 
 import java.util.*;
 import java.util.stream.Collectors;
@@ -40,7 +39,7 @@ public class JavaApplication {
 
 
         // 파일 저장 방식
-        // userCRUDTest((BasicUserService)userServiceJCF);
+        // userCRUDTest((BasicUserService)userServiceFile);
         // messageCRUDTest((BasicMessageService) messageServiceFile);
         channelCRUDTest((BasicChannelService) channelServiceFile);
 
@@ -53,52 +52,70 @@ public class JavaApplication {
         System.out.println("User 5명 생성이 완료되었습니다.");
         System.out.println("---------------------------");
 
-        List<String> usernames = Arrays.asList("Wooody", "Alice", "Bob", "Eve", "Malary");
-        List<User> users = usernames.stream()
-                .map(userService::createUser)
+        List<User> createUsers = Arrays.asList(
+                new User("Woody", "woody@gmail.com", "1234"),
+                new User("Alice", "alice@gmail.com", "1234"),
+                new User("Bob", "bob@gmail.com", "1234"),
+                new User("Eve", "eve@gmail.com", "1234"),
+                new User("Joy", "joy@gmail.com", "1234")
+        );
+
+        // userService에 저장
+        List<User> users  = createUsers.stream()
+                .map(user -> userService.createUser(user.getUsername(), user.getEmail(), user.getPassword()))
                 .collect(Collectors.toList());
 
         for (User user : users) {
-            System.out.println("유저 생성 : " + user);
+            System.out.println("유저 생성 : " + user.getUsername() + ", 이메일 : " + user.getEmail());
         }
         System.out.println();
 
         // 단건 조회
         Optional<User> findUser = userService.getUser(users.get(2).getId());
-        System.out.println("찾은 사람 : " + findUser + "\n");
+        findUser.ifPresent(user -> {
+            System.out.println("찾은 사람 : " + user.getUsername() + ", 이메일 : "+ user.getEmail() + "\n");
+        });
+
 
         // 사용자 이름 수정
-        User user1 = users.get(0);
-        System.out.println("수정 전 이름 : " + user1.getUsername() + ", 생성시간 : " + user1.getCreatedAt());
-        user1.update("Buzz");
-        userService.updateUser(user1.getId(), user1); // Woody -> Buzz 으로 이름 변경
-        System.out.println("수정 후 이름 : " + user1.getUsername() + ", 수정시간 : " + user1.getUpdatedAt());
+        User updatedUser = users.get(0);
+        System.out.println("수정 전 이름 : " + updatedUser.getUsername() + ", 생성시간 : " + updatedUser.getCreatedAt());
+        updatedUser.update("Buzz", "buzz@gmail.com","1234");
+        userService.updateUser(updatedUser.getId(), updatedUser); // Woody -> Buzz 으로 이름 변경
+        System.out.println("수정 후 이름 : " + updatedUser.getUsername() + ", 수정시간 : " + updatedUser.getUpdatedAt());
         System.out.println();
+
 
         // 모든 사용자 조회
         System.out.println("====== 전체 유저 조회  =====");
         System.out.println("총 유저 수 : " + userService.getAllUsers().size());
-        System.out.println(userService.getAllUsers());
+        // System.out.println(userService.getAllUsers());
+        for (User user : users) {
+            System.out.println("유저 이름 : " + user.getUsername() + ", 이메일 : " + user.getEmail());
+        }
         System.out.println();
 
 
         // 사용자 삭제
         System.out.println("====== 유저 삭제 ======");
-        User user2 = users.get(1);
-        userService.deleteUser(user2.getId());
-        System.out.println("삭제된 유저 이름 : " + user2.getUsername());
+        User deletedUser = users.get(1);
+        userService.deleteUser(deletedUser.getId());
+        System.out.println("삭제된 유저 이름 : " + deletedUser.getUsername());
         System.out.println();
+
+        // 삭제 후 전체 유저 조회
         System.out.println("-- 삭제 후 전체 유저 조회 --");
-        System.out.println("현재 유저 수 : " + users.size());
-        for (User user : users) {
-            System.out.println(user);
+        List<User> users1 = userService.getAllUsers();
+        System.out.println("현재 유저 수 : " + users1.size());
+        for (User user : users1) {
+            System.out.println("유저 이름 : " + user.getUsername() + ", 이메일 : " + user.getEmail());
         }
         System.out.println();
 
         // 사용자 존재 여부 확인
         System.out.println("====== 유저 존재 여부 확인  ======");
-        boolean result = userService.existsById(user2.getId());
-        System.out.println("'" + user2.getUsername() + "' 의 존재 여부 확인 : " + result);
+        boolean result = userService.existsById(deletedUser.getId());
+        System.out.println("'" + deletedUser.getUsername() + "' 의 존재 여부 확인 : " + result);
         User user3 = users.get(2);
         boolean result1 = userService.existsById(user3.getId());
         System.out.println("'" + user3.getUsername() + "' 의 존재 여부 확인 : " + result1);
@@ -113,10 +130,13 @@ public class JavaApplication {
         System.out.println("---------------------------");
         System.out.println();
 
-        List<String> messagesContent = Arrays.asList("Hello Java World!", "Hello Java World!", "Good Morning!");
-        List<Message> messages = messagesContent.stream()
-                        .map(messageService::createMessage)
-                        .collect(Collectors.toList());
+
+        List<String> createMessages = Arrays.asList(
+                "Hello Java World!", "Happy Birthday", "Good Morning!"
+        );
+        List<Message> messages = createMessages.stream()
+                .map(content -> messageService.createMessage(content, UUID.randomUUID(), UUID.randomUUID()))
+                .collect(Collectors.toList());
 
         for (Message message : messages) {
             System.out.println("메세지 생성 : " + message.getContent());
@@ -125,42 +145,50 @@ public class JavaApplication {
 
 
         // 단건 조회
-        Message message1 = messages.get(0);
-        Optional<Message> findMessage = messageService.getMessage(message1.getId());
-        System.out.println("찾은 메세지 : " + findMessage.get().getContent() + "\n");
+        Optional<Message> findMessage = messageService.getMessage(messages.get(0).getId());
+        findMessage.ifPresent(message -> {
+            System.out.println("찾은 메세지 : " + message.getContent() + "\n");
+        });
+
 
         // 메세지 내용 수정
-        System.out.println("수정 전 내용 : " + message1.getContent() + ", 생성시간 : " + message1.getCreatedAt());
-        message1.update("Merry Christmas");
-        messageService.updateMessage(message1.getId(),"Merry Christmas"); // Happy Birthday -> Merry Christmas 으로 메세지 변경
-        System.out.println("수정 후 내용 : " + message1.getContent() + ", 수정시간 : " + message1.getUpdatedAt());
+        Message updatedMessage = messages.get(1);
+        System.out.println("수정 전 내용 : " + updatedMessage.getContent() + ", 생성시간 : " + updatedMessage.getCreatedAt());
+        messageService.updateMessage(updatedMessage.getId(),"Merry Christmas"); // Happy Birthday -> Merry Christmas 으로 메세지 변경
+        System.out.println("수정 후 내용 : " + updatedMessage.getContent() + ", 수정시간 : " + updatedMessage.getUpdatedAt());
         System.out.println();
+
 
         // 모든 메세지 조회
         System.out.println("====== 전체 메세지 조회  =====");
-        System.out.println("총 Message 개수 : " + messageService.getAllMessages().size());
-        System.out.println(messageService.getAllMessages());
+        List<Message> messages1 = messageService.getAllMessages();
+        System.out.println("총 메세지 개수 : " + messages1.size());
+        // System.out.println(messageService.getAllMessages());
+        for (Message message : messages1) {
+            System.out.println("메세지 : " + message.getContent() + ", 채널ID : " + message.getChannelId() + ", 글쓴이ID : " + message.getAuthorId());
+        }
         System.out.println();
 
 
         // 메세지 삭제
         System.out.println("** 메세지가 삭제되었습니다 **");
-        Message message3 = messages.get(2);
-        messageService.deleteMessage(message3.getId());
-        System.out.println("✅ 삭제된 Message 내용 : " + message3.getContent() + "\n");
+        Message deletedMessage = messages.get(2);
+        messageService.deleteMessage(deletedMessage.getId());
+        System.out.println("✅ 삭제된 Message 내용 : " + deletedMessage.getContent() + "\n");
 
         System.out.println("-- 삭제 후 전체 메세지 조회 --");
-        System.out.println("현재 메세지 개수 : " + messages.size());
-        for (Message message : messages) {
-            System.out.println(message);
+        List<Message> messages2 = messageService.getAllMessages();
+        System.out.println("현재 메세지 개수 : " + messages2.size());
+        for (Message message : messages2) {
+            System.out.println("메세지 : " + message.getContent() + ", 채널ID : " + message.getChannelId() + ", 글쓴이ID : " + message.getAuthorId());
         }
         System.out.println();
 
         // 메세지 존재 여부 확인
         System.out.println("====== 메세지 존재 여부 확인  ======");
-        Message message2 = messages.get(1);
-        boolean result = messageService.existsById(message2.getId());
-        System.out.println("'" + message2.getContent() + "' 의 존재 여부 확인 : " + result);
+        boolean result = messageService.existsById(deletedMessage.getId());
+        System.out.println("'" + deletedMessage.getContent() + "' 의 존재 여부 확인 : " + result);
+        Message message3 = messages.get(1);
         boolean result1 = messageService.existsById(message3.getId());
         System.out.println("'" + message3.getContent() + "' 의 존재 여부 확인 : " + result1);
     }
@@ -172,41 +200,54 @@ public class JavaApplication {
         System.out.println("---------------------------");
         System.out.println();
 
-        List<String> channelName = Arrays.asList("공지", "문의", "학습", "스터디");
-        List<Channel> channels = channelName.stream()
-                .map(channelService::createChannel)
+        List<Channel> createChannels = Arrays.asList(
+                new Channel(ChannelType.PUBLIC, "공지", "공지 채널입니다"),
+                new Channel(ChannelType.PUBLIC, "문의", "문의 채널입니다"),
+                new Channel(ChannelType.PUBLIC, "학습", "학습 채널입니다"),
+                new Channel(ChannelType.PRIVATE, "스터디", "스터디 채널입니다")
+        );
+
+        // channelService에 저장
+        List<Channel> channels = createChannels.stream()
+                .map(channel -> channelService.create(channel.getType(), channel.getChannelname(), channel.getDescription()))
                 .collect(Collectors.toList());
+
         for (Channel channel : channels) {
-            System.out.println("채널 생성 : " + channel.getChannelname());
+            System.out.println("채널 생성 : " + channel.getChannelname() + ", 채널 설명 : " + channel.getDescription());
         }
         System.out.println();
 
 
         // 채널 단건 조회
-        Channel channel2 = channels.get(1);
-        Optional<Channel> findChannel = channelService.getChannel(channel2.getId());
-        System.out.println("찾은 채널 : " + findChannel.get().getChannelname() + "\n");
-
-
-        // 채널 제목 수정
-        System.out.println("수정 전 채널명 : " + channel2.getChannelname() + ", 생성시간 : " + channel2.getCreatedAt());
-        channelService.updateChannel(channel2.getId(), "자유"); // 학습 -> 자유 로 채널명 변경
-        System.out.println("수정 후 채널명 : " + channel2.getChannelname() + ", 수정시간 : " + channel2.getUpdatedAt());
-        System.out.println();
+        Optional<Channel> findChannel = channelService.find(channels.get(1).getId());
+        findChannel.ifPresent(channel -> {
+            System.out.println("찾은 채널 : " + channel.getChannelname() + ", 채널 설명 : " + channel.getDescription() + "\n");
+        });
 
         // 모든 채널 조회
         System.out.println("====== 전체 채널 조회  =====");
-        System.out.println("총 채널 개수 : " + channelService.getAllChannels().size());
-        System.out.println(channelService.getAllChannels());
+        List<Channel> channels1 = channelService.findAll();
+        System.out.println("총 채널 개수 : " + channels1.size());
+        // System.out.println(channelService.findAll());
+        for (Channel channel : channels1) {
+            System.out.println("채널명 : " + channel.getChannelname() + ", 채널설명 : "+ channel.getDescription());
+        }
         System.out.println();
 
+
         // 채널 삭제
-        Channel channel3 = channels.get(2);
-        System.out.println("** 채널 <" + channel3.getChannelname() + "> 이 삭제되었습니다 **");
-        channelService.deleteChannel(channel3.getId());
-        System.out.println("삭제된 Channel 이름 : " + channel3.getChannelname() + "\n");
-        System.out.println("-- 삭제 후 전체 전체 조회 --");
-        System.out.println("현재 Channel 개수 : " + channelService.getAllChannels().size());
-        System.out.println(channelService.getAllChannels());
+        Channel deletedChannel = channels.get(2);
+        System.out.println("** 채널 <" + deletedChannel.getChannelname() + "> 이 삭제되었습니다 **");
+        channelService.delete(deletedChannel.getId());
+        System.out.println("삭제된 Channel 이름 : " + deletedChannel.getChannelname() + "\n");
+
+        // 채널 삭제 후 전체 채널 조회
+        System.out.println("-- 삭제 후 전체 채널 조회 --");
+        List<Channel> channels2 = channelService.findAll();
+        System.out.println("현재 Channel 개수 : " + channels2.size());
+        for (Channel channel : channels2) {
+            System.out.println("채널명 : " + channel.getChannelname() + ", 채널설명 : "+ channel.getDescription());
+        }
+
     }
 }
