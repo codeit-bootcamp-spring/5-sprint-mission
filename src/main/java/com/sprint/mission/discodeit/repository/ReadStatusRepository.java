4a49842186cd3252edd6a319af456f795ed0dad8
yelim-Lee <dpfla1078@naver.com>
2025-08-05 18:33:25 +0900

    refactor: add annotation

--- src/main/java/com/sprint/mission/discodeit/repository/ReadStatusRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/ReadStatusRepository.java
@@ -1,12 +1,14 @@
 package com.sprint.mission.discodeit.repository;
 
 import com.sprint.mission.discodeit.entity.ReadStatus;
+import org.springframework.stereotype.Service;
 
 import java.time.Instant;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+@Service
 public interface ReadStatusRepository {
     void save(ReadStatus readStatus);
     Optional<ReadStatus> findById(UUID userId, UUID channelId);
