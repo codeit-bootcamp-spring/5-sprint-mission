16e213c29e37835083a52049f6e6f2d7fc17a5ff
jeonggiju <halogiju123@gmail.com>
2025-08-05 17:13:14 +0900

    etc: 깃허브 테스트용 커밋

--- src/main/java/com/sprint/mission/discodeit/entity/BaseEntity.java
+++ src/main/java/com/sprint/mission/discodeit/entity/BaseEntity.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 
 public class BaseEntity implements Serializable {
     private static final long serialVersionUID = 1L;
+    private static final long test = 1;
 
     private final UUID id;
     private final Long createdAt;
