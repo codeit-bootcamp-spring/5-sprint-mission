f423ad9def8694a2c95c517bfcee62950ac17f4f
yelim-Lee <dpfla1078@naver.com>
2025-08-05 18:36:43 +0900

    refactor : create 메서드 분리 - public - private

--- src/main/java/com/sprint/mission/discodeit/service/basic/BasicChannelService.java
+++ src/main/java/com/sprint/mission/discodeit/service/basic/BasicChannelService.java
@@ -1,12 +1,20 @@
 package com.sprint.mission.discodeit.service.basic;
 
+import com.sprint.mission.discodeit.dto.request.PrivateChannelCreateRequest;
+import com.sprint.mission.discodeit.dto.request.PublicChannelCreateRequest;
+import com.sprint.mission.discodeit.dto.response.ChannelFindResponse;
 import com.sprint.mission.discodeit.entity.Channel;
 import com.sprint.mission.discodeit.entity.ChannelType;
+import com.sprint.mission.discodeit.entity.ReadStatus;
+import com.sprint.mission.discodeit.entity.User;
 import com.sprint.mission.discodeit.repository.ChannelRepository;
+import com.sprint.mission.discodeit.repository.MessageRepository;
+import com.sprint.mission.discodeit.repository.ReadStatusRepository;
 import com.sprint.mission.discodeit.service.ChannelService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.UUID;
@@ -15,17 +23,42 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class BasicChannelService implements ChannelService {
     private final ChannelRepository channelRepository;
+    private final ReadStatusRepository readStatusRepository;
+    private final MessageRepository messageRepository;
 
     @Override
-    public Channel create(ChannelType type, String name, String description) {
-        Channel channel = new Channel(type, name, description);
-        return channelRepository.save(channel);
+    public Channel createPublic(PublicChannelCreateRequest request) {
+
+        Channel channel = new Channel(ChannelType.PUBLIC, request.name(), request.description());
+        channelRepository.save(channel);
+        return channel;
     }
 
+    // PRIVATE 채널을 생성할 때:
+    // [ ] 채널에 참여하는 User의 정보를 받아 User 별 ReadStatus 정보를 생성합니다.
+    // [ ] name과 description 속성은 생략합니다.
     @Override
-    public Channel find(UUID channelId) {
-        return channelRepository.findById(channelId)
-                        .orElseThrow(() -> new NoSuchElementException("Channel with id " + channelId + " not found"));
+    public Channel createPrivate(PrivateChannelCreateRequest request) {
+
+        Channel channel = new Channel(ChannelType.PRIVATE, null,null);
+        channelRepository.save(channel);
+
+        for (User user : request.user()) {
+            ReadStatus readStatus = new ReadStatus(user.getId(), channel.getId());
+            readStatusRepository.save(readStatus);
+        }
+        return channel;
+    }
+
+    //해당 채널의 가장 최근 메시지의 시간 정보를 포함합니다.
+    //PRIVATE 채널인 경우 참여한 User의 id 정보를 포함합니다.
+    @Override
+    public ChannelFindResponse find(UUID channelId) {
+
+        Channel channel = channelRepository.findById(channelId)
+                .orElseThrow(()-> new NoSuchElementException("Channel with id " + channelId + " not found"));
+
+        Instant messageTime = messageRepository.
     }
 
     @Override
