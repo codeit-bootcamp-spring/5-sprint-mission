f5d9deca9677c8d042b798d20524a2241c1c821f
kimyumin03 <alsfla03@gmail.com>
2025-08-07 09:49:51 +0900

    sprint mission3: userStatus implementation

---
+++ src/main/java/com/sprint/mission/discodeit/entity/UserStatus.java
@@ -0,0 +1,21 @@
+package com.sprint.mission.discodeit.entity;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.UUID;
+
+@Getter
+@RequiredArgsConstructor
+public class UserStatus {
+    private final UUID id;
+    private final UUID userId;
+    private final Instant createdAt;
+    private Instant lastAccessedAt;
+
+    public boolean isOnline() {
+        return Duration.between(lastAccessedAt, Instant.now()).toMinutes() <= 5;
+    }
+}
---
+++ src/main/java/com/sprint/mission/discodeit/repository/UserStatusRepository.java
@@ -0,0 +1,11 @@
+package com.sprint.mission.discodeit.repository;
+
+import com.sprint.mission.discodeit.entity.UserStatus;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserStatusRepository {
+    void save(UserStatus userStatus);
+    Optional<UserStatus> findByUserId(UUID userId);
+}
