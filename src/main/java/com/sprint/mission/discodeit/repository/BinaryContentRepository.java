786bb3f0c99da64923fe30714ffc6b7ec30ba6dc
polodumbo <polosh1208@gmail.com>
2025-08-07 16:45:04 +0900

    feat: BinaryContent 도메인, 레포지토리 클래스 작성

---
+++ src/main/java/com/sprint/mission/discodeit/repository/BinaryContentRepository.java
@@ -0,0 +1,19 @@
+package com.sprint.mission.discodeit.repository;
+
+import com.sprint.mission.discodeit.entity.BinaryContent;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface BinaryContentRepository {
+    BinaryContent save(BinaryContent binaryContent);
+
+    Optional<BinaryContent> findById(UUID binaryContentId);
+
+    List<BinaryContent> findAll();
+
+    boolean existsById(UUID binaryContentId);
+
+    boolean delete(UUID binaryContentId);
+}
