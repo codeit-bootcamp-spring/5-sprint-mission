94ced11e57ad27635723a5dc3e08a67bc34cd0f3
yelim-Lee <dpfla1078@naver.com>
2025-08-07 11:07:36 +0900

    feat : add findLatestByChannelId 메서드

--- src/main/java/com/sprint/mission/discodeit/repository/MessageRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/MessageRepository.java
@@ -2,6 +2,7 @@ package com.sprint.mission.discodeit.repository;
 
 import com.sprint.mission.discodeit.entity.Message;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -10,6 +11,7 @@ public interface MessageRepository {
     Message save(Message message);
     Optional<Message> findById(UUID id);
     List<Message> findAll();
+    Optional<Message> findLatestByChannelId(UUID channelId);
     boolean existsById(UUID id);
     void deleteById(UUID id);
 }
