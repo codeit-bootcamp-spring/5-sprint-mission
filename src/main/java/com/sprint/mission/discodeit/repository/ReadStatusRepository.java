651396b97e83b9ad0b1c5b68fb35e3e17543849f
kimyumin03 <alsfla03@gmail.com>
2025-08-07 14:44:54 +0900

    sprint mission3: Edit for your channel

--- src/main/java/com/sprint/mission/discodeit/repository/ReadStatusRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/ReadStatusRepository.java
@@ -10,4 +10,6 @@ public interface ReadStatusRepository {
     void save(ReadStatus readStatus);
     Optional<ReadStatus> findByUserIdAndChannelId(UUID userId, UUID channelId);
     List<ReadStatus> findAllByUserId(UUID userId);
+    List<ReadStatus> findAllByChannelId(UUID channelId);
+    void deleteAllByChannelId(UUID channelId);
 }
