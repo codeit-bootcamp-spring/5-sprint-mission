0e82258d6078616e9a45d7e2cda6229825e61d43
jeonggiju <halogiju123@gmail.com>
2025-08-08 14:45:56 +0900

    add: FileBinaryContentRepository 추가

---
+++ src/main/java/com/sprint/mission/discodeit/repository/file/FileBinaryContentRepository.java
@@ -0,0 +1,78 @@
+package com.sprint.mission.discodeit.repository.file;
+
+import com.sprint.mission.discodeit.entity.BinaryContent;
+import com.sprint.mission.discodeit.repository.BinaryContentRepository;
+import com.sprint.mission.discodeit.util.FileUtil;
+import org.springframework.stereotype.Repository;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public class FileBinaryContentRepository implements BinaryContentRepository {
+    private final Path directoryPath = Path.of(FileUtil.getBasePath() +"/binary_contents" );
+
+
+    @Override
+    public Optional<BinaryContent> save(BinaryContent content) {
+        if(content == null){
+            return Optional.empty();
+        }
+
+        Path filePath = Path.of(directoryPath.toAbsolutePath() + "/" + content.getId() + FileUtil.getExtension());
+        FileUtil.saveEntity(filePath, content);
+
+        return Optional.of(content);
+    }
+
+    @Override
+    public Optional<BinaryContent> findById(UUID id) {
+        Path path = Path.of(directoryPath.toAbsolutePath() + "/" + id + FileUtil.getExtension());
+        return FileUtil.loadEntity(path, BinaryContent.class);
+    }
+
+    @Override
+    public List<BinaryContent> findAll() {
+        File directory = new File(directoryPath.toAbsolutePath() + "/");
+
+        if(!directory.exists() || !directory.isDirectory()){
+            return List.of();
+        }
+
+        File[] files = directory.listFiles();
+        List<BinaryContent> contents = new ArrayList<>();
+
+        if(files == null){
+            return contents;
+        }
+
+        for(File file : files){
+            if(file.isFile() && file.getName().endsWith(FileUtil.getExtension())){
+                contents.add(FileUtil.loadEntity(file.toPath(), BinaryContent.class).orElseThrow());
+            }
+        }
+        return contents;
+    }
+
+    @Override
+    public void deleteById(UUID id) {
+        Path path = Path.of(directoryPath.toAbsolutePath() + "/" + id + FileUtil.getExtension());
+        path.toFile().delete();
+    }
+
+    @Override
+    public void deleteAll() {
+        File directory = new File(directoryPath.toAbsolutePath() + "/");
+
+        File[] files = directory.listFiles();
+        if(files != null){
+            for(File file : files){
+                file.delete();
+            }
+        }
+    }
+}
--- src/main/java/com/sprint/mission/discodeit/repository/jcf/JCFBinaryContentRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/jcf/JCFBinaryContentRepository.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
-@Repository
+//@Repository
 public class JCFBinaryContentRepository implements BinaryContentRepository {
     Map<UUID, BinaryContent> data = new HashMap<>();
 
