4302f7d09a620dc7a231c19479fac3ff90609f4d
BakJiSeok <commicat2@gmail.com>
2025-08-09 20:51:08 +0900

    feat: `@ConfigurationProperties` 추가 및 Bean 등록
    - `AppMetadataProperties`, `AppStorageProperties`, `AppServerProperties`에 `@ConfigurationProperties` 추가.
    - 관련 Bean 등록을 위한 `AppConfig` 업데이트.

--- src/main/java/com/sprint/mission/discodeit/config/AppConfig.java
+++ src/main/java/com/sprint/mission/discodeit/config/AppConfig.java
@@ -5,6 +5,17 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
+
+    @Bean
+    public AppServerProperties appServerProperties(AppProperties appProperties) {
+        return appProperties.server();
+    }
+
+    @Bean
+    AppMetadataProperties appMetadataProperties(AppProperties appProperties) {
+        return appProperties.metadata();
+    }
+
     @Bean
     public AppStorageProperties appStorageProperties(AppProperties appProperties) {
         return appProperties.storage();
--- src/main/java/com/sprint/mission/discodeit/config/AppMetadataProperties.java
+++ src/main/java/com/sprint/mission/discodeit/config/AppMetadataProperties.java
@@ -1,4 +1,7 @@
 package com.sprint.mission.discodeit.config;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "app.metadata")
 public record AppMetadataProperties(String region, String owner) {
 }
