3bf2164e65e9bf4d2981c15609264d392a059b2c
sooyeonz <5196ksy@naver.com>
2025-08-10 17:40:48 +0900

    feat: binarycontent interface, jcf, file 구현

---
+++ src/main/java/com/codeit/mission/discodeit/repository/BinaryContentRepository.java
@@ -0,0 +1,19 @@
+package com.codeit.mission.discodeit.repository;
+
+import com.codeit.mission.discodeit.entity.BinaryContent;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface BinaryContentRepository {
+    BinaryContent save(BinaryContent binaryContent);
+
+    Optional<BinaryContent> findById(UUID id);
+
+    List<BinaryContent> findAll();
+
+    boolean existsById(UUID id);
+
+    void deleteById(UUID id);
+}
