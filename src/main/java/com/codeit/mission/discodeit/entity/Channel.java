15cd05a82780fc330221e7f242b5fa54de4e4121
sooyeonz <5196ksy@naver.com>
2025-08-10 17:22:25 +0900

    feat: 새로운 엔티티 추가 및 변경

---
+++ src/main/java/com/codeit/mission/discodeit/entity/Channel.java
@@ -0,0 +1,48 @@
+package com.codeit.mission.discodeit.entity;
+
+import lombok.Getter;
+import lombok.ToString;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.UUID;
+
+@Getter
+@ToString
+public class Channel implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private UUID id;
+    private Instant createdAt;
+    private Instant updatedAt;
+
+    private ChannelType type;
+    private String name;
+    private String description;
+
+    public Channel(ChannelType type, String name, String description) {
+        this.id = UUID.randomUUID();
+        this.createdAt = Instant.now();
+
+        this.type = type;
+        this.name = name;
+        this.description = description;
+    }
+
+    public void update(String newName, String newDescription) {
+        boolean anyValueUpdated = false;
+        if (newName != null && !newName.equals(this.name)) {
+            this.name = newName;
+            anyValueUpdated = true;
+        }
+        if (newDescription != null && !newDescription.equals(this.description)) {
+            this.description = newDescription;
+            anyValueUpdated = true;
+        }
+
+        if (anyValueUpdated) {
+            this.updatedAt = Instant.now();
+        }
+    }
+}
---
+++ src/main/java/com/codeit/mission/discodeit/entity/User.java
@@ -0,0 +1,54 @@
+package com.codeit.mission.discodeit.entity;
+
+import lombok.Getter;
+import lombok.ToString;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.UUID;
+
+@Getter
+@ToString
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private UUID id;
+    private Instant createdAt;
+    private Instant updatedAt;
+
+    private String username;
+    private String email;
+    private String password;
+
+    private UUID profileId;
+
+    public User(String username, String email, String password) {
+        this.id = UUID.randomUUID();
+        this.createdAt = Instant.now();
+
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public void update(String newUsername, String newEmail, String newPassword) {
+        boolean anyValueUpdated = false;
+        if (newUsername != null && !newUsername.equals(this.username)) {
+            this.username = newUsername;
+            anyValueUpdated = true;
+        }
+        if (newEmail != null && !newEmail.equals(this.email)) {
+            this.email = newEmail;
+            anyValueUpdated = true;
+        }
+        if (newPassword != null && !newPassword.equals(this.password)) {
+            this.password = newPassword;
+            anyValueUpdated = true;
+        }
+
+        if (anyValueUpdated) {
+            this.updatedAt = Instant.now();
+        }
+    }
+}
