3f3ac6effc9d3d36f53514f04ea1d2bc6ec7c48f
JaehyeokLim <217686834+JaehyeokLim@users.noreply.github.com>
2025-08-12 00:49:07 +0900

    feat: 도메인 확장 - BinaryContent - ReadStatus - UserStatus

---
+++ src/main/java/com/sprint/mission/discodeit/entity/sub/ReadStatus.java
@@ -0,0 +1,45 @@
+package com.sprint.mission.discodeit.entity.sub;
+
+import lombok.Getter;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Getter
+public class ReadStatus {
+    private static final long serialVersionUID = 1L;
+
+    private final UUID id;
+    private final UUID userId;
+    private final UUID channelId;
+
+    private final Instant createdAt;
+    private Instant updatedAt;
+    private Instant lastReadAt;
+
+    public ReadStatus(UUID userId, UUID channelId, Instant lastReadAt) {
+        this.id = UUID.randomUUID();
+        this.createdAt = Instant.now();
+
+        this.userId = userId;
+        this.channelId = channelId;
+        this.lastReadAt = lastReadAt;
+    }
+
+    public void update(Instant newLastReadAt) {
+        boolean anyValueUpdated = false;
+
+        if (isLasReadAtChanged(newLastReadAt)) {
+            this.lastReadAt = newLastReadAt;
+            anyValueUpdated = true;
+        }
+
+        if (anyValueUpdated) {
+            this.updatedAt = Instant.now();
+        }
+    }
+
+    private boolean isLasReadAtChanged(Instant newLastReadAt) {
+        return newLastReadAt != null && !newLastReadAt.equals(this.lastReadAt);
+    }
+}
