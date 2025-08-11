03d8ba63d346b073823c062835ec79d823c34a5e
JaehyeokLim <217686834+JaehyeokLim@users.noreply.github.com>
2025-08-12 01:43:49 +0900

    feat: Message DTO 추가 - MessageCreateRequest - MessageUpdateRequest

---
+++ src/main/java/com/sprint/mission/discodeit/dto/message/request/MessageCreateRequest.java
@@ -0,0 +1,9 @@
+package com.sprint.mission.discodeit.dto.message.request;
+
+import java.util.UUID;
+
+public record MessageCreateRequest(
+        String content,
+        UUID channelId,
+        UUID authorId
+) {}
---
+++ src/main/java/com/sprint/mission/discodeit/dto/message/request/MessageUpdateRequest.java
@@ -0,0 +1,5 @@
+package com.sprint.mission.discodeit.dto.message.request;
+
+public record MessageUpdateRequest(
+        String newContent
+) {}
