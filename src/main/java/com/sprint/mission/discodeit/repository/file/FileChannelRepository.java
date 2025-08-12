2c1b467e6dddc4ecf15d379f4e7cfea4e32c8a8d
김규섭 <pens9053@gmail.com>
2025-08-12 13:43:36 +0900

    refactor: FileChannelRepository.java 에서 데이터를 불러오는 방식에 대한 수정 기존 FindAll로 매번 디스크에서 데이터를 불러 와 새로운 list를 만들던 방식에서, 레포지토리 내부에 필드를 만들어 객체 생성 시 최초 한 번만 불러온 뒤 해당 객체를 사용하는 방식으로 변경

--- src/main/java/com/sprint/mission/discodeit/repository/file/FileChannelRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/file/FileChannelRepository.java
@@ -2,6 +2,7 @@ package com.sprint.mission.discodeit.repository.file;
 
 import com.sprint.mission.discodeit.configuration.RepositoryProps;
 import com.sprint.mission.discodeit.entity.Channel;
+import com.sprint.mission.discodeit.exception.ThrowableIOException;
 import com.sprint.mission.discodeit.repository.ChannelRepository;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Repository;
@@ -10,15 +11,15 @@ import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
+import java.util.*;
+import java.util.stream.Stream;
 
 @Repository
 @ConditionalOnProperty(name = "discodeit.repository.type", havingValue = "file")
 public class FileChannelRepository implements ChannelRepository {
     private final Path DIRECTORY;
     private final String EXTENSION = ".ser";
+    private final Map<UUID, Channel> channelMap = new HashMap<>();
 
     public FileChannelRepository(RepositoryProps props) {
         Path root = Paths.get(props.getFileDirectory());
@@ -30,13 +31,35 @@ public class FileChannelRepository implements ChannelRepository {
             try {
                 Files.createDirectories(DIRECTORY);
             } catch (IOException e) {
-                throw new RuntimeException(e);
+                throw new ThrowableIOException("디렉토리 생성 실패 : " + DIRECTORY, e);
             }
         }
+        load();
     }
 
     private Path resolvePath(UUID id) {
         return DIRECTORY.resolve(id + EXTENSION);
+    }
+
+    private void load() {
+        try (Stream<Path> paths = Files.list(DIRECTORY)) {
+            List<Channel> list = paths
+                    .filter(path -> path.toString().endsWith(EXTENSION))
+                    .map(path -> {
+                        try (
+                                FileInputStream fis = new FileInputStream(path.toFile());
+                                ObjectInputStream ois = new ObjectInputStream(fis)
+                        ) {
+                            return (Channel) ois.readObject();
+                        } catch (IOException | ClassNotFoundException e) {
+                            throw new ThrowableIOException("불러오기 실패 : " + path, e);
+                        }
+                    })
+                    .toList();
+            list.forEach(channel -> channelMap.put(channel.getId(), channel));
+        } catch (IOException e) {
+            throw new ThrowableIOException("불러오기 실패 : " + DIRECTORY, e);
+        }
     }
 
     @Override
@@ -47,63 +70,36 @@ public class FileChannelRepository implements ChannelRepository {
                 ObjectOutputStream oos = new ObjectOutputStream(fos)
         ) {
             oos.writeObject(channel);
+            channelMap.put(channel.getId(), channel);
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new ThrowableIOException("저장 실패 : " + path, e);
         }
         return channel;
     }
 
     @Override
     public Optional<Channel> findById(UUID id) {
-        Channel channelNullable = null;
-        Path path = resolvePath(id);
-        if (Files.exists(path)) {
-            try (
-                    FileInputStream fis = new FileInputStream(path.toFile());
-                    ObjectInputStream ois = new ObjectInputStream(fis)
-            ) {
-                channelNullable = (Channel) ois.readObject();
-            } catch (IOException | ClassNotFoundException e) {
-                throw new RuntimeException(e);
-            }
-        }
-        return Optional.ofNullable(channelNullable);
+        return Optional.ofNullable(channelMap.get(id));
     }
 
     @Override
     public List<Channel> findAll() {
-        try {
-            return Files.list(DIRECTORY)
-                    .filter(path -> path.toString().endsWith(EXTENSION))
-                    .map(path -> {
-                        try (
-                                FileInputStream fis = new FileInputStream(path.toFile());
-                                ObjectInputStream ois = new ObjectInputStream(fis)
-                        ) {
-                            return (Channel) ois.readObject();
-                        } catch (IOException | ClassNotFoundException e) {
-                            throw new RuntimeException(e);
-                        }
-                    })
-                    .toList();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        return List.copyOf(channelMap.values());
     }
 
     @Override
     public boolean existsById(UUID id) {
-        Path path = resolvePath(id);
-        return Files.exists(path);
+        return channelMap.containsKey(id);
     }
 
     @Override
     public void deleteById(UUID id) {
         Path path = resolvePath(id);
         try {
-            Files.delete(path);
+            Files.deleteIfExists(path);
+            channelMap.remove(id);
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new ThrowableIOException("삭제 실패 : " + path, e);
         }
     }
 }
