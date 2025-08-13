5b263d057cd7b0422f71edd2e302c8d27cb92bc9
sooyeonz <5196ksy@naver.com>
2025-08-13 11:26:22 +0900

    fix: 모범코드 반영 - message 관련 dto 추가

---
+++ src/main/java/com/codeit/mission/discodeit/dto/request/MessageCreateRequest.java
@@ -0,0 +1,10 @@
+package com.codeit.mission.discodeit.dto.request;
+
+import java.util.UUID;
+
+public record MessageCreateRequest(
+        String content,
+        UUID channelId,
+        UUID authorId
+) {
+}
