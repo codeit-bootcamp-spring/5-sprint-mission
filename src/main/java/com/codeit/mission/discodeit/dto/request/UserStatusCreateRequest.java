ff18f9c860a83d351ce2eee26e91ea922c91acde
sooyeonz <5196ksy@naver.com>
2025-08-13 11:34:45 +0900

    fix: 모범코드 반영 - userstatus 관련 dto 추가

---
+++ src/main/java/com/codeit/mission/discodeit/dto/request/UserStatusCreateRequest.java
@@ -0,0 +1,10 @@
+package com.codeit.mission.discodeit.dto.request;
+
+import java.time.Instant;
+import java.util.UUID;
+
+public record UserStatusCreateRequest(
+        UUID userId,
+        Instant lastActiveAt
+) {
+}
