f8b78fd2bd660059af22be511163c0def2df7cdd
김규섭 <pens9053@gmail.com>
2025-08-13 17:25:21 +0900

    fix: @NotBlank를 @NotNull과 @NotEmpty로 변경

--- src/main/java/com/sprint/mission/discodeit/dto/request/PrivateChannelCreateRequest.java
+++ src/main/java/com/sprint/mission/discodeit/dto/request/PrivateChannelCreateRequest.java
@@ -1,11 +1,12 @@
 package com.sprint.mission.discodeit.dto.request;
 
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 
 import java.util.List;
 import java.util.UUID;
 
 public record PrivateChannelCreateRequest(
-        @NotBlank List<UUID> userIds
+        @NotNull @NotEmpty List<UUID> userIds
 ) {
 }
