3e059918bddd2721ea36dc04a4f7a0f11d09c6fd
JaehyeokLim <217686834+JaehyeokLim@users.noreply.github.com>
2025-08-13 19:52:31 +0900

    feat: UserStatusRepository 인터페이스 및 구현체 구현 - JcfUserRepository - FileUserRepository

---
+++ src/main/java/com/sprint/mission/discodeit/repository/jcf/JCFUserStatusRepository.java
@@ -0,0 +1,53 @@
+package com.sprint.mission.discodeit.repository.jcf;
+
+import com.sprint.mission.discodeit.entity.sub.UserStatus;
+import com.sprint.mission.discodeit.repository.UserStatusRepository;
+
+import java.util.*;
+
+public class JCFUserStatusRepository implements UserStatusRepository {
+    private final Map<UUID, UserStatus> data;
+
+    public JCFUserStatusRepository() {
+        this.data = new HashMap<>();
+    }
+
+    @Override
+    public UserStatus save(UserStatus userStatus) {
+        this.data.put(userStatus.getId(), userStatus);
+        return userStatus;
+    }
+
+    @Override
+    public Optional<UserStatus> findById(UUID id) {
+        return Optional.ofNullable(this.data.get(id));
+    }
+
+    @Override
+    public Optional<UserStatus> findByUserId(UUID userId) {
+        return this.findAll().stream()
+                .filter(userStatus -> userStatus.getUserId().equals(userId))
+                .findFirst();
+    }
+
+    @Override
+    public List<UserStatus> findAll() {
+        return this.data.values().stream().toList();
+    }
+
+    @Override
+    public boolean existsById(UUID id) {
+        return this.data.containsKey(id);
+    }
+
+    @Override
+    public void deleteById(UUID id) {
+        this.data.remove(id);
+    }
+
+    @Override
+    public void deleteByUserId(UUID userId) {
+        this.findByUserId(userId)
+                .ifPresent(userStatus -> this.deleteByUserId(userStatus.getId()));
+    }
+}
