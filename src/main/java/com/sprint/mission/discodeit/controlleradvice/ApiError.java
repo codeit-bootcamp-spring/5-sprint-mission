8d69b7f312621542b500017bddf376abb16910fb
BakJiSeok <commicat2@gmail.com>
2025-08-15 19:15:53 +0900

    feat: ApiError 클래스에 timestamp 필드 추가 및 생성자 수정

--- src/main/java/com/sprint/mission/discodeit/controlleradvice/ApiError.java
+++ src/main/java/com/sprint/mission/discodeit/controlleradvice/ApiError.java
@@ -1,6 +1,15 @@
 package com.sprint.mission.discodeit.controlleradvice;
 
+import java.time.Instant;
 import java.util.List;
 
-public record ApiError(String code, String message, List<String> errors) {
+public record ApiError(
+        Instant timestamp,
+        String code,
+        String message,
+        List<String> errors) {
+
+    public ApiError(String code, String message, List<String> errors) {
+        this(Instant.now(), code, message, errors);
+    }
 }
