804d6ee96ad278800c40f1469eb4e2bd13625f3c
김규섭 <pens9053@gmail.com>
2025-08-15 16:45:27 +0900

    feat: BinaryContentController.java의 find 메서드가 바로 파일을 표시하게끔 수정, 그에 따라 script.js 역시 알맞게 수정

--- src/main/java/com/sprint/mission/discodeit/controller/BinaryContentController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/BinaryContentController.java
@@ -3,12 +3,11 @@ package com.sprint.mission.discodeit.controller;
 import com.sprint.mission.discodeit.entity.BinaryContent;
 import com.sprint.mission.discodeit.service.basic.BasicBinaryContentService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
@@ -19,9 +18,15 @@ import java.util.UUID;
 public class BinaryContentController {
     private final BasicBinaryContentService binaryContentService;
 
-    @RequestMapping(path = "/find")
-    public ResponseEntity<BinaryContent> find(@RequestParam("binaryContentId") UUID binaryContentId) {
-        BinaryContent binaryContent = binaryContentService.findById(binaryContentId);
-        return ResponseEntity.status(HttpStatus.OK).body(binaryContent);
+    @RequestMapping(path = "/find",
+            method = RequestMethod.GET)
+    public ResponseEntity<byte[]> find(@RequestParam("binaryContentId") UUID binaryContentId) {
+        BinaryContent file = binaryContentService.findById(binaryContentId);
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .contentType(MediaType.parseMediaType(file.getContentType()))
+                .header(HttpHeaders.CONTENT_DISPOSITION,
+                        "inline; filename=\"" + file.getFileName() + "\"")
+                .body(file.getBytes());
     }
 }
