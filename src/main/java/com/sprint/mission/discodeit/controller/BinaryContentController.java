77c145d0428ca01eaa2a4a25ae0c0e052f3de752
김규섭 <pens9053@gmail.com>
2025-08-19 14:34:27 +0900

    feat: findAllByIdIn 메서드 추가, find 메서드와 파라미터의 어노테이션 변경

--- src/main/java/com/sprint/mission/discodeit/controller/BinaryContentController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/BinaryContentController.java
@@ -2,15 +2,18 @@ package com.sprint.mission.discodeit.controller;
 
 import com.sprint.mission.discodeit.entity.BinaryContent;
 import com.sprint.mission.discodeit.service.basic.BasicBinaryContentService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.List;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -18,15 +21,15 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestController
 @RestControllerAdvice
 @RequiredArgsConstructor
-@RequestMapping("/api/binaryContent")
+@RequestMapping("/api/binaryContents")
 @Tag(name = "BinaryContent", description = "첨부파일 API")
 public class BinaryContentController {
 
   private final BasicBinaryContentService binaryContentService;
 
-  @RequestMapping(path = "/find",
-      method = RequestMethod.GET)
-  public ResponseEntity<byte[]> find(@RequestParam("binaryContentId") UUID binaryContentId) {
+  @Operation(summary = "첨부 파일 조회")
+  @GetMapping("/{binaryContentId}")
+  public ResponseEntity<byte[]> find(@PathVariable("binaryContentId") UUID binaryContentId) {
     BinaryContent file = binaryContentService.findById(binaryContentId);
 
     return ResponseEntity.status(HttpStatus.OK)
@@ -34,5 +37,14 @@ public class BinaryContentController {
         .header(HttpHeaders.CONTENT_DISPOSITION,
             "inline; filename=\"" + file.getFileName() + "\"")
         .body(file.getBytes());
+  }
+
+  @Operation(summary = "여러 첨부 파일 조회")
+  @GetMapping
+  public ResponseEntity<List<BinaryContent>> findAllByIdIn(
+      @RequestParam List<UUID> binaryContentIds) {
+    List<BinaryContent> files = binaryContentService.findAllByIdIn(binaryContentIds);
+
+    return ResponseEntity.status(HttpStatus.OK).body(files);
   }
 }
