291d32487407eea16c0ed00adff6d713430867e0
김규섭 <pens9053@gmail.com>
2025-08-19 14:27:35 +0900

    fix: openApi 메서드 Bean 등록

--- src/main/java/com/sprint/mission/discodeit/configuration/OpenApiConfig.java
+++ src/main/java/com/sprint/mission/discodeit/configuration/OpenApiConfig.java
@@ -4,12 +4,13 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
 import java.util.List;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenApiConfig {
 
-
+  @Bean
   public OpenAPI openApi() {
     return new OpenAPI()
         .info(new Info()
