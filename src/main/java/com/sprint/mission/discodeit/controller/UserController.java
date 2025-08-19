0772c157a03e7ce537a1d77a81840c685252b355
j2eun0922 <j2eun0922@naver.com>
2025-08-19 18:59:48 +0900

    feat: implement user updateLogin REST API

--- src/main/java/com/sprint/mission/discodeit/controller/UserController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/UserController.java
@@ -4,10 +4,12 @@ import com.sprint.mission.discodeit.dto.api.ApiResult;
 import com.sprint.mission.discodeit.dto.data.UserDto;
 import com.sprint.mission.discodeit.dto.request.BinaryContentCreateRequest;
 import com.sprint.mission.discodeit.dto.request.UserCreateRequest;
+import com.sprint.mission.discodeit.dto.request.UserStatusUpdateRequest;
 import com.sprint.mission.discodeit.dto.request.UserUpdateRequest;
 import com.sprint.mission.discodeit.entity.User;
-import com.sprint.mission.discodeit.service.BinaryContentService;
+import com.sprint.mission.discodeit.entity.UserStatus;
 import com.sprint.mission.discodeit.service.UserService;
+import com.sprint.mission.discodeit.service.UserStatusService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -25,7 +27,7 @@ import java.util.UUID;
 @AllArgsConstructor
 public class UserController {
     private final UserService userService;
-    private final BinaryContentService binaryContentService;
+    private final UserStatusService userStatusService;
 
     @RequestMapping(value = "/register", method = RequestMethod.POST,
             consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
@@ -65,14 +67,23 @@ public class UserController {
     }
 
     @RequestMapping(value = "/delete/{id}", method = RequestMethod.DELETE)
-    public ResponseEntity<ApiResult<String>> userDelete(@PathVariable("id") UUID id){
+    public ResponseEntity<ApiResult<String>> userDelete(@PathVariable("id") UUID id) {
         userService.delete(id);
-        return ResponseEntity.status(HttpStatus.OK).body(ApiResult.ok("사용자 "+id+" 가 삭제되었습니다"));
+        return ResponseEntity.status(HttpStatus.OK).body(ApiResult.ok("사용자 " + id + " 가 삭제되었습니다"));
     }
 
     @RequestMapping(value = "/findAll", method = RequestMethod.GET)
-    public ResponseEntity<ApiResult<List<UserDto>>> findAllUser(){
+    public ResponseEntity<ApiResult<List<UserDto>>> findAllUser() {
         List<UserDto> users = userService.findAll();
         return ResponseEntity.status(HttpStatus.OK).body(ApiResult.ok(users));
+    }
+
+    @RequestMapping(value = "/updateLogin/{id}", method = RequestMethod.PATCH,
+            consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<ApiResult<String>> userUpdateLogin(
+            @PathVariable("id") UUID id,
+            @RequestBody UserStatusUpdateRequest userStatusUpdateRequest) {
+        UserStatus userStatus = userStatusService.updateByUserId(id, userStatusUpdateRequest);
+        return ResponseEntity.status(HttpStatus.OK).body(ApiResult.ok("사용자 " + id + " 의 상태가 변경되었습니다"));
     }
 }
--- src/main/java/com/sprint/mission/discodeit/dto/data/UserDto.java
+++ src/main/java/com/sprint/mission/discodeit/dto/data/UserDto.java
@@ -1,8 +1,11 @@
 package com.sprint.mission.discodeit.dto.data;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import java.time.Instant;
 import java.util.UUID;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public record UserDto(
         UUID id,
         Instant createdAt,
