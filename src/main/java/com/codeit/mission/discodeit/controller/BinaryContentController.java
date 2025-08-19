b71d358378c06a1bf75593af0465a63ec8c50d5d
sooyeonz <5196ksy@naver.com>
2025-08-19 15:27:38 +0900

    feat: swagger ui - binarycontent 테스트를 위한 어노테이션 추가

--- src/main/java/com/codeit/mission/discodeit/controller/BinaryContentController.java
+++ src/main/java/com/codeit/mission/discodeit/controller/BinaryContentController.java
@@ -2,6 +2,10 @@ package com.codeit.mission.discodeit.controller;
 
 import com.codeit.mission.discodeit.entity.BinaryContent;
 import com.codeit.mission.discodeit.service.BinaryContentService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import java.util.UUID;
@@ -23,14 +27,21 @@ public class BinaryContentController {
     private final BinaryContentService binaryContentService;
 
     @GetMapping("/{binaryContentId}")
+    @Operation(summary = "단건 파일 조회", description = "한 개의 파일을 가져옵니다.")
     public ResponseEntity<BinaryContent> find(@PathVariable UUID binaryContentId) {
         BinaryContent binaryContent = binaryContentService.find(binaryContentId);
         return ResponseEntity.status(HttpStatus.OK).body(binaryContent);
     }
 
     @GetMapping
+    @Operation(summary = "다건 파일 조회", description = "전체 파일의 목록을 가져옵니다.")
     public ResponseEntity<List<BinaryContent>> findAllByIdIn(
-        @RequestParam List<UUID> binaryContentIds) {
+        @RequestParam
+        @Parameter(
+            description = "binaryContentIds",
+            required = true
+        )
+        List<UUID> binaryContentIds) {
         if (binaryContentIds == null || binaryContentIds.isEmpty()) {
             throw new IllegalArgumentException("binaryContentIds가 필요합니다.");
         }
