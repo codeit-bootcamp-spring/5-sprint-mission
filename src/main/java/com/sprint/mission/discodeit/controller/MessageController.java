176122108f67edd0b255ee8aee304dc79072cde8
j2eun0922 <j2eun0922@naver.com>
2025-08-20 17:43:36 +0900

    feat: implement message update and delete REST API

--- src/main/java/com/sprint/mission/discodeit/controller/MessageController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/MessageController.java
@@ -3,20 +3,19 @@ package com.sprint.mission.discodeit.controller;
 import com.sprint.mission.discodeit.dto.api.ApiResult;
 import com.sprint.mission.discodeit.dto.request.BinaryContentCreateRequest;
 import com.sprint.mission.discodeit.dto.request.MessageCreateRequest;
+import com.sprint.mission.discodeit.dto.request.MessageUpdateRequest;
 import com.sprint.mission.discodeit.entity.Message;
 import com.sprint.mission.discodeit.service.MessageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/message")
@@ -27,7 +26,7 @@ public class MessageController {
     @RequestMapping(value = "/send", method = RequestMethod.POST,
             consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<ApiResult<Message>> messageSend(@RequestPart("message") MessageCreateRequest messageCreateRequest,
-                                                           @RequestPart(required = false, value = "attachment") List<MultipartFile> attachment) throws IOException {
+                                                          @RequestPart(required = false, value = "attachment") List<MultipartFile> attachment) throws IOException {
         List<BinaryContentCreateRequest> binaryContent = List.of();
 
         if (attachment != null && !attachment.isEmpty()) {
@@ -37,7 +36,7 @@ public class MessageController {
                             return new BinaryContentCreateRequest(
                                     file.getName(), file.getContentType(), file.getBytes()
                             );
-                        }catch (IOException e) {
+                        } catch (IOException e) {
                             throw new RuntimeException(e);
                         }
                     }).toList();
@@ -45,5 +44,19 @@ public class MessageController {
 
         Message message = messageService.create(messageCreateRequest, binaryContent);
         return ResponseEntity.status(HttpStatus.CREATED).body(ApiResult.ok(message, "메시지가 전송되었습니다"));
+    }
+
+    @RequestMapping(value = "/update/{id}", method = RequestMethod.POST,
+            consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<ApiResult<Message>> messageUpdate(@PathVariable("id") UUID id,
+                                                            @RequestBody MessageUpdateRequest messageUpdateRequest) {
+        Message message = messageService.update(id, messageUpdateRequest);
+        return ResponseEntity.status(HttpStatus.OK).body(ApiResult.ok(message, "메시지 " + id + "가 수정되었습니다"));
+    }
+
+    @RequestMapping(value = "/delete/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<ApiResult<Message>> messageUpdate(@PathVariable("id") UUID id) {
+        messageService.delete(id);
+        return ResponseEntity.status(HttpStatus.OK).body(ApiResult.ok("메시지 " + id + "가 삭제되었습니다"));
     }
 }
