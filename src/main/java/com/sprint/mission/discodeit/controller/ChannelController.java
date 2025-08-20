d9b09e60d6b7852a641a9690ba70f075d5e5f314
j2eun0922 <j2eun0922@naver.com>
2025-08-20 10:16:50 +0900

    feat: implement channel update REST API

--- src/main/java/com/sprint/mission/discodeit/controller/ChannelController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/ChannelController.java
@@ -1,19 +1,18 @@
 package com.sprint.mission.discodeit.controller;
 
-
 import com.sprint.mission.discodeit.dto.api.ApiResult;
 import com.sprint.mission.discodeit.dto.request.PrivateChannelCreateRequest;
 import com.sprint.mission.discodeit.dto.request.PublicChannelCreateRequest;
+import com.sprint.mission.discodeit.dto.request.PublicChannelUpdateRequest;
 import com.sprint.mission.discodeit.entity.Channel;
 import com.sprint.mission.discodeit.service.ChannelService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/channel")
@@ -33,5 +32,14 @@ public class ChannelController {
     public ResponseEntity<ApiResult<Channel>> privateChannelCreate(@RequestBody PrivateChannelCreateRequest createRequest) {
         Channel channel = channelService.create(createRequest);
         return ResponseEntity.status(HttpStatus.CREATED).body(ApiResult.ok(channel, "비공개 채널이 생성되었습니다"));
+    }
+
+    @RequestMapping(value = "/update/{id}", method = RequestMethod.PATCH,
+            consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<ApiResult<Channel>> channelUpdate(
+            @PathVariable UUID id,
+            @RequestBody PublicChannelUpdateRequest updateRequest) {
+        Channel channel = channelService.update(id, updateRequest);
+        return ResponseEntity.status(HttpStatus.OK).body(ApiResult.ok(channel, "채널 "+channel.getId()+"이 수정되었습니다"));
     }
 }
