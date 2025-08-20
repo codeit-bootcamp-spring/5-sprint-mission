d6609a891ab62deee153386db2800821cec3f768
j2eun0922 <j2eun0922@naver.com>
2025-08-20 10:49:10 +0900

    feat: implement channel delete and find REST API

--- src/main/java/com/sprint/mission/discodeit/controller/ChannelController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/ChannelController.java
@@ -1,6 +1,7 @@
 package com.sprint.mission.discodeit.controller;
 
 import com.sprint.mission.discodeit.dto.api.ApiResult;
+import com.sprint.mission.discodeit.dto.data.ChannelDto;
 import com.sprint.mission.discodeit.dto.request.PrivateChannelCreateRequest;
 import com.sprint.mission.discodeit.dto.request.PublicChannelCreateRequest;
 import com.sprint.mission.discodeit.dto.request.PublicChannelUpdateRequest;
@@ -12,6 +13,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -36,10 +38,21 @@ public class ChannelController {
 
     @RequestMapping(value = "/update/{id}", method = RequestMethod.PATCH,
             consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<ApiResult<Channel>> channelUpdate(
-            @PathVariable UUID id,
-            @RequestBody PublicChannelUpdateRequest updateRequest) {
+    public ResponseEntity<ApiResult<Channel>> channelUpdate(@PathVariable("id") UUID id,
+                                                            @RequestBody PublicChannelUpdateRequest updateRequest) {
         Channel channel = channelService.update(id, updateRequest);
-        return ResponseEntity.status(HttpStatus.OK).body(ApiResult.ok(channel, "채널 "+channel.getId()+"이 수정되었습니다"));
+        return ResponseEntity.status(HttpStatus.OK).body(ApiResult.ok(channel, "채널 " + channel.getId() + "이 수정되었습니다"));
+    }
+
+    @RequestMapping(value = "/delete/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<ApiResult<Channel>> channelDelete(@PathVariable("id") UUID id) {
+        channelService.delete(id);
+        return ResponseEntity.status(HttpStatus.OK).body(ApiResult.ok("채널 " + id + "이 삭제되었습니다"));
+    }
+
+    @RequestMapping(value = "find/{userId}", method = RequestMethod.GET)
+    public ResponseEntity<ApiResult<List<ChannelDto>>> channelFindAllByUserId(@PathVariable("userId") UUID userId) {
+        List<ChannelDto> channels = channelService.findAllByUserId(userId);
+        return ResponseEntity.status(HttpStatus.OK).body(ApiResult.ok(channels));
     }
 }
