0bdb19e223ea14c58eda86ca514239bb20df5cee
backKim1024 <dydgml7313@naver.com>
2025-08-21 18:55:40 +0900

    refactor(ReadStatus) : Change findAllByUserId endpoint to RESTful style(GET /api/readStatues)
    ReadStatusController의 findAllByUserId 메서드를 RESTful 스타일로 변경
    - 기존
    + @RequestMapping(path = "findAllByUserId")
    - 변경
    + @GetMapping

--- src/main/java/com/sprint/mission/discodeit/controller/ReadStatusController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/ReadStatusController.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -44,7 +45,7 @@ public class ReadStatusController {
         .body(updatedReadStatus);
   }
 
-  @RequestMapping(path = "findAllByUserId")
+  @GetMapping
   public ResponseEntity<List<ReadStatus>> findAllByUserId(@RequestParam("userId") UUID userId) {
     List<ReadStatus> readStatuses = readStatusService.findAllByUserId(userId);
     return ResponseEntity
