e01a2ec5184fdf1cd49ebc41f1d980ad7bfde89f
backKim1024 <dydgml7313@naver.com>
2025-08-21 18:41:17 +0900

    refactor(Message) : Change update endpoint to RESTful style(Patch /api/messages)
    MessageController의 각각의 update 메서드를 RESTful 스타일로 변경
    @PathVariable 활용
    - 기존
    +   @RequestMapping(path = "delete")
    - 변경
    + @DeleteMapping("/{messageId}")
    + delete(@PathVariable UUID messageId)

--- src/main/java/com/sprint/mission/discodeit/controller/MessageController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/MessageController.java
@@ -14,6 +14,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -66,8 +67,8 @@ public class MessageController {
         .body(updatedMessage);
   }
 
-  @RequestMapping(path = "delete")
-  public ResponseEntity<Void> delete(@RequestParam("messageId") UUID messageId) {
+  @DeleteMapping("/{messageId}")
+  public ResponseEntity<Void> delete(@PathVariable UUID messageId) {
     messageService.delete(messageId);
     return ResponseEntity
         .status(HttpStatus.NO_CONTENT)
