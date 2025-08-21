aa44fe74461bf74bb89f7d83022f51cd06022f6d
backKim1024 <dydgml7313@naver.com>
2025-08-21 18:57:39 +0900

    refactor(User) : Change findAll endpoint to RESTful style(GET /api/users)
    UserController의 findAll 메서드를 RESTful 스타일로 변경
    - 기존
    + @RequestMapping(path = "findAll")
    - 변경
    + @GetMapping

--- src/main/java/com/sprint/mission/discodeit/controller/UserController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.sprint.mission.discodeit.controller;
 
+import com.sprint.mission.discodeit.dto.common.ApiResult;
 import com.sprint.mission.discodeit.dto.data.UserDto;
 import com.sprint.mission.discodeit.dto.request.BinaryContentCreateRequest;
 import com.sprint.mission.discodeit.dto.request.UserCreateRequest;
@@ -9,37 +10,45 @@ import com.sprint.mission.discodeit.entity.User;
 import com.sprint.mission.discodeit.entity.UserStatus;
 import com.sprint.mission.discodeit.service.UserService;
 import com.sprint.mission.discodeit.service.UserStatusService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
-@RequiredArgsConstructor
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/users")
+@Tag(name = "User", description = "User API")
 public class UserController {
 
   private final UserService userService;
   private final UserStatusService userStatusService;
 
   // 전체조회
+  @Operation(summary = "전체 User 목록 조회", operationId = "findAll")
   @GetMapping
-  public ResponseEntity<List<UserDto>> findAll() {
+  public ResponseEntity<List<UserDto>> findAll(
+      @RequestParam(required = false) String username) {
     return ResponseEntity.ok(userService.findAll());
   }
 
-  @RequestMapping(
-      path = "create",
-      consumes = {MediaType.MULTIPART_FORM_DATA_VALUE}
-  )
+  // 생성
+  @Operation(summary = "User 등록", operationId = "create")
+  @PostMapping(consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
   public ResponseEntity<User> create(
       @RequestPart("userCreateRequest") UserCreateRequest userCreateRequest,
       @RequestPart(value = "profile", required = false) MultipartFile profile
@@ -51,6 +60,7 @@ public class UserController {
         .status(HttpStatus.CREATED)
         .body(createdUser);
   }
+
 
   @RequestMapping(
       path = "update",
