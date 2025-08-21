235d796e9de7c97b4e1cd76bddc46fcaff6d6b4c
backKim1024 <dydgml7313@naver.com>
2025-08-21 18:08:16 +0900

    refactor(auth) : Change findAll endpoint to RESTful style(POST /api/auth)
    AuthController의 login을 RESTful 스타일에 맞게 수정
    - 기존: @RequestMapping(path = "login")
    - 변경: @PostMapping("/login")

--- src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
@@ -6,20 +6,19 @@ import com.sprint.mission.discodeit.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @RequiredArgsConstructor
-@Controller
-@ResponseBody
 @RequestMapping("/api/auth")
 public class AuthController {
 
   private final AuthService authService;
 
-  @RequestMapping(path = "login")
+  @PostMapping("/login")
   public ResponseEntity<User> login(@RequestBody LoginRequest loginRequest) {
     User user = authService.login(loginRequest);
     return ResponseEntity
