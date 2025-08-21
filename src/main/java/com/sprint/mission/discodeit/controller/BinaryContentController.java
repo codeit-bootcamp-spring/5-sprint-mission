35ce75e7c5bae9e1fb779806077336644dfb5b37
backKim1024 <dydgml7313@naver.com>
2025-08-21 18:15:40 +0900

    refactor(binarycontent) : Change find endpoint to RESTful style(GET /api/binaryContent)
    BinaryContentController의 find를 RESTful 스타일에 맞게 수정하고, @PathVariable를 활용하여 URL 경로에 있는 값을 메서드 파라미터로 바인딩
    - 기존: @RequestMapping(path = "find")
    - 변경: @PostMapping("/{binaryContentId}")
    find (@PathVariable UUID binaryContentId)

--- src/main/java/com/sprint/mission/discodeit/controller/BinaryContentController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/BinaryContentController.java
@@ -2,27 +2,26 @@ package com.sprint.mission.discodeit.controller;
 
 import com.sprint.mission.discodeit.entity.BinaryContent;
 import com.sprint.mission.discodeit.service.BinaryContentService;
+import java.util.List;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-import java.util.UUID;
-
+@RestController
 @RequiredArgsConstructor
-@Controller
-@ResponseBody
 @RequestMapping("/api/binaryContent")
 public class BinaryContentController {
 
   private final BinaryContentService binaryContentService;
 
-  @RequestMapping(path = "find")
-  public ResponseEntity<BinaryContent> find(@RequestParam("binaryContentId") UUID binaryContentId) {
+  @GetMapping("/{binaryContentId}")
+  public ResponseEntity<BinaryContent> find(@PathVariable UUID binaryContentId) {
     BinaryContent binaryContent = binaryContentService.find(binaryContentId);
     return ResponseEntity
         .status(HttpStatus.OK)
