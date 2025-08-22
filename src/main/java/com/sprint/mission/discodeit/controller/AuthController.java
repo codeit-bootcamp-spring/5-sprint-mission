322408e105447e42d1b8099d782f3b4b05fe79f4
kimyumin03 <alsfla03@gmail.com>
2025-08-22 10:40:56 +0900

    sprint mission5: AuthContentController implementation

--- src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
@@ -3,27 +3,26 @@ package com.sprint.mission.discodeit.controller;
 import com.sprint.mission.discodeit.dto.request.LoginRequest;
 import com.sprint.mission.discodeit.entity.User;
 import com.sprint.mission.discodeit.service.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "Auth")
 @RequiredArgsConstructor
-@Controller
-@ResponseBody
+@RestController
 @RequestMapping("/api/auth")
 public class AuthController {
 
   private final AuthService authService;
 
-  @RequestMapping(path = "login")
-  public ResponseEntity<User> login(@RequestBody LoginRequest loginRequest) {
+  @Operation(summary = "로그인")
+  @PostMapping(path = "/login", consumes = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<User> login(@Valid @RequestBody LoginRequest loginRequest) {
     User user = authService.login(loginRequest);
-    return ResponseEntity
-        .status(HttpStatus.OK)
-        .body(user);
+    return ResponseEntity.ok(user);
   }
 }
