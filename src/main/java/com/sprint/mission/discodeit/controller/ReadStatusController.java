53e49f6cf022f7a83aab349af63e14059b00e8f0
backKim1024 <dydgml7313@naver.com>
2025-08-25 10:34:55 +0900

    docs(readStatus): API 문서화를 위한 @Tag, @Operation 어노테이션 추가

--- src/main/java/com/sprint/mission/discodeit/controller/ReadStatusController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/ReadStatusController.java
@@ -4,6 +4,8 @@ import com.sprint.mission.discodeit.dto.request.ReadStatusCreateRequest;
 import com.sprint.mission.discodeit.dto.request.ReadStatusUpdateRequest;
 import com.sprint.mission.discodeit.entity.ReadStatus;
 import com.sprint.mission.discodeit.service.ReadStatusService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,10 +26,12 @@ import java.util.UUID;
 @Controller
 @ResponseBody
 @RequestMapping("/api/readStatuses")
+@Tag(name = "readStatus", description = "ReadStatus API")
 public class ReadStatusController {
 
   private final ReadStatusService readStatusService;
 
+  @Operation(summary = "읽음 상태 생성", operationId = "create")
   @PostMapping
   public ResponseEntity<ReadStatus> create(@RequestBody ReadStatusCreateRequest request) {
     ReadStatus createdReadStatus = readStatusService.create(request);
@@ -36,6 +40,7 @@ public class ReadStatusController {
         .body(createdReadStatus);
   }
 
+  @Operation(summary = "읽음 상태 수정", operationId = "update")
   @PatchMapping("/{readStatusId}")
   public ResponseEntity<ReadStatus> update(@PathVariable UUID readStatusId,
       @RequestBody ReadStatusUpdateRequest request) {
@@ -45,6 +50,7 @@ public class ReadStatusController {
         .body(updatedReadStatus);
   }
 
+  @Operation(summary = "읽음 상태 조회", operationId = "findAllByUserId")
   @GetMapping
   public ResponseEntity<List<ReadStatus>> findAllByUserId(@RequestParam("userId") UUID userId) {
     List<ReadStatus> readStatuses = readStatusService.findAllByUserId(userId);
