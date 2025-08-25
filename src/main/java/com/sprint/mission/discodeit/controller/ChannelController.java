1eb9d10b369ce4834813717e09f1c72a768e5524
backKim1024 <dydgml7313@naver.com>
2025-08-25 10:39:36 +0900

    docs(channel): API 문서화를 위한 @Tag, @Operation 어노테이션 추가

--- src/main/java/com/sprint/mission/discodeit/controller/ChannelController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/ChannelController.java
@@ -6,6 +6,8 @@ import com.sprint.mission.discodeit.dto.request.PublicChannelCreateRequest;
 import com.sprint.mission.discodeit.dto.request.PublicChannelUpdateRequest;
 import com.sprint.mission.discodeit.entity.Channel;
 import com.sprint.mission.discodeit.service.ChannelService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
@@ -24,10 +26,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/channels")
+@Tag(name = "Channel", description = "Channel API")
 public class ChannelController {
 
   private final ChannelService channelService;
 
+  @Operation(summary = "publicChannel 생성", operationId = "publicCreate")
   @PostMapping("/public")
   public ResponseEntity<Channel> create(@RequestBody PublicChannelCreateRequest request) {
     Channel createdChannel = channelService.create(request);
@@ -36,6 +40,7 @@ public class ChannelController {
         .body(createdChannel);
   }
 
+  @Operation(summary = "privateChannel 생성", operationId = "privateCreate")
   @PostMapping("/private")
   public ResponseEntity<Channel> create(@RequestBody PrivateChannelCreateRequest request) {
     Channel createdChannel = channelService.create(request);
@@ -44,6 +49,7 @@ public class ChannelController {
         .body(createdChannel);
   }
 
+  @Operation(summary = "Channel 수정", operationId = "update")
   @PatchMapping("/{channelId}")
   public ResponseEntity<Channel> update(@PathVariable UUID channelId,
       @RequestBody PublicChannelUpdateRequest request) {
@@ -53,6 +59,7 @@ public class ChannelController {
         .body(udpatedChannel);
   }
 
+  @Operation(summary = "Channel 삭제", operationId = "delete")
   @DeleteMapping("/{channelId}")
   public ResponseEntity<Void> delete(@PathVariable UUID channelId) {
     channelService.delete(channelId);
@@ -61,6 +68,7 @@ public class ChannelController {
         .build();
   }
 
+  @Operation(summary = "User에 연결된 Channel 전체조회", operationId = "findAll")
   @GetMapping
   public ResponseEntity<List<ChannelDto>> findAll(@RequestParam("userId") UUID userId) {
     List<ChannelDto> channels = channelService.findAllByUserId(userId);
