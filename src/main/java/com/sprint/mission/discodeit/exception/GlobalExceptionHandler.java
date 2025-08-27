caca12a81058a0a498a055688e76a4a54b7da407
chagyuhwan <agyuccim@gmail.com>
2025-08-27 12:19:26 +0900

    refactor: @RestControllerAdvice로 변경

--- src/main/java/com/sprint/mission/discodeit/exception/GlobalExceptionHandler.java
+++ src/main/java/com/sprint/mission/discodeit/exception/GlobalExceptionHandler.java
@@ -2,13 +2,13 @@ package com.sprint.mission.discodeit.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.NoSuchElementException;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+@RestControllerAdvice
 @ResponseBody
 public class GlobalExceptionHandler {
 
