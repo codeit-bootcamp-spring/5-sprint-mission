bc85c2fd5251250df35d356675abe9bd5fbc327e
the-eris-net <the.eris.net@gmail.com>
2025-08-27 18:59:21 +0900

    feat:add

--- src/main/java/com/sprint/mission/discodeit/repository/jcf/JcfChannelRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/jcf/JcfChannelRepository.java
@@ -2,47 +2,44 @@ package com.sprint.mission.discodeit.repository.jcf;
 
 import com.sprint.mission.discodeit.entity.Channel;
 import com.sprint.mission.discodeit.repository.ChannelRepository;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
-public class JcfChannelRepository implements ChannelRepository {
-    // 사용자 데이터를 저장할 HashMap. UUID를 키로, Channel 객체를 값으로 사용
-    private final Map<UUID, Channel> channels = new HashMap<>();
+@ConditionalOnProperty(name = "discodeit.repository.type", havingValue = "jcf", matchIfMissing = true)
+@Repository
+public class JCFChannelRepository implements ChannelRepository {
 
+  private final Map<UUID, Channel> data;
 
-    @Override
-    public Channel save(Channel channel) {
-        channels.put(channel.getId(), channel);
-        System.out.println("[Repo]Channel saved to JCF cache: " + channel.getId());
-        return channel;
-    }
+  public JCFChannelRepository() {
+    this.data = new HashMap<>();
+  }
 
-    @Override
-    public Optional<Channel> findById(UUID id) {
-        System.out.println("[Repo]Finding Channel by ID in JCF cache: " + id);
-        return Optional.ofNullable(channels.get(id));
-    }
+  @Override
+  public Channel save(Channel channel) {
+    this.data.put(channel.getId(), channel);
+    return channel;
+  }
 
-    @Override
-    public Optional<Channel> findByName(String name) {
-        System.out.println("[Repo]Finding channel by email in JCF cache: " + name);
-        return channels.values().stream()
-                .filter(channel -> channel.getChannelName() != null && channel.getChannelName().equals(name))
-                .findFirst(); //
-    }
+  @Override
+  public Optional<Channel> findById(UUID id) {
+    return Optional.ofNullable(this.data.get(id));
+  }
 
-    @Override
-    public List<Channel> findAll() {
-        System.out.println("[Repo]Retrieving all channel from JCF cache. Total: " + channels.size());
-        return new ArrayList<>(channels.values());
-    }
+  @Override
+  public List<Channel> findAll() {
+    return this.data.values().stream().toList();
+  }
 
-    @Override
-    public void delete(UUID id) {
-        if(!channels.containsKey(id)) {
-            throw new NoSuchElementException("[Repo]Channel with id " + id + " not found");
-        }
-        channels.remove(id);
-        System.out.println("[Repo]Channel deleted from JCF cache: "+ id);
-    }
+  @Override
+  public boolean existsById(UUID id) {
+    return this.data.containsKey(id);
+  }
+
+  @Override
+  public void deleteById(UUID id) {
+    this.data.remove(id);
+  }
 }
