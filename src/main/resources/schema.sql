f601ee7f8c6608967e6bc890fe359d1e1d423366
김규섭 <pens9053@gmail.com>
2025-08-29 14:50:45 +0900

    chore: 데이터베이스 스키마 추가, JPA와 PostgreSQL을 위한 의존성 추가, 데이터베이스 연결과 SQL 로깅을 위한 설정값 추가

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,118 @@
+CREATE TABLE "users"
+(
+    "id"         uuid         NOT NULL,
+    "profile_id" uuid         NULL,
+    "created_at" timestamptz  NOT NULL,
+    "updated_at" timestamptz  NULL,
+    "username"   VARCHAR(50)  NOT NULL UNIQUE,
+    "email"      VARCHAR(100) NOT NULL UNIQUE,
+    "password"   VARCHAR(60)  NOT NULL
+);
+
+CREATE TYPE "channel_type" AS ENUM ('PUBLIC', 'PRIVATE');
+
+CREATE TABLE "channels"
+(
+    "id"          uuid         NOT NULL,
+    "created_at"  timestamptz  NOT NULL,
+    "updated_at"  timestamptz  NULL,
+    "name"        VARCHAR(100) NULL,
+    "description" VARCHAR(500) NULL,
+    "type"        channel_type NOT NULL
+);
+
+CREATE TABLE "messages"
+(
+    "id"         uuid        NOT NULL,
+    "channel_id" uuid        NOT NULL,
+    "author_id"  uuid        NULL,
+    "created_at" timestamptz NOT NULL,
+    "updated_at" timestamptz NULL,
+    "content"    TEXT        NULL
+);
+
+CREATE TABLE "binary_contents"
+(
+    "id"           uuid         NOT NULL,
+    "created_at"   timestamptz  NOT NULL,
+    "file_name"    VARCHAR(255) NOT NULL,
+    "size"         BIGINT       NOT NULL,
+    "content_type" VARCHAR(100) NOT NULL,
+    "bytes"        bytea        NOT NULL
+);
+
+CREATE TABLE "message_attachments"
+(
+    "message_id"    uuid NULL,
+    "attachment_id" uuid NULL
+);
+
+CREATE TABLE "user_statuses"
+(
+    "id"             uuid        NOT NULL,
+    "user_id"        uuid        NOT NULL UNIQUE,
+    "created_at"     timestamptz NOT NULL,
+    "updated_at"     timestamptz NULL,
+    "last_active_at" timestamptz NOT NULL
+);
+
+CREATE TABLE "read_statuses"
+(
+    "id"           uuid        NOT NULL,
+    "user_id"      uuid        NOT NULL,
+    "channel_id"   uuid        NOT NULL,
+    "created_at"   timestamptz NOT NULL,
+    "updated_at"   timestamptz NULL,
+    "last_read_at" timestamptz NOT NULL,
+    UNIQUE ("user_id", "channel_id")
+);
+
+ALTER TABLE "users"
+    ADD CONSTRAINT "PK_USERS" PRIMARY KEY ("id");
+
+ALTER TABLE "channels"
+    ADD CONSTRAINT "PK_CHANNELS" PRIMARY KEY ("id");
+
+ALTER TABLE "messages"
+    ADD CONSTRAINT "PK_MESSAGES" PRIMARY KEY ("id");
+
+ALTER TABLE "binary_contents"
+    ADD CONSTRAINT "PK_BINARY_CONTENTS" PRIMARY KEY ("id");
+
+ALTER TABLE "user_statuses"
+    ADD CONSTRAINT "PK_USER_STATUSES" PRIMARY KEY ("id");
+
+ALTER TABLE "read_statuses"
+    ADD CONSTRAINT "PK_READ_STATUSES" PRIMARY KEY ("id");
+
+ALTER TABLE "users"
+    ADD CONSTRAINT "FK_binary_contents_TO_users_1" FOREIGN KEY ("profile_id")
+        REFERENCES "binary_contents" ("id") ON DELETE SET NULL;
+
+ALTER TABLE "messages"
+    ADD CONSTRAINT "FK_channels_TO_messages_1" FOREIGN KEY ("channel_id")
+        REFERENCES "channels" ("id") ON DELETE CASCADE;
+
+ALTER TABLE "messages"
+    ADD CONSTRAINT "FK_users_TO_messages_1" FOREIGN KEY ("author_id")
+        REFERENCES "users" ("id") ON DELETE SET NULL;
+
+ALTER TABLE "message_attachments"
+    ADD CONSTRAINT "FK_messages_TO_message_attachments_1" FOREIGN KEY ("message_id")
+        REFERENCES "messages" ("id") ON DELETE CASCADE;
+
+ALTER TABLE "message_attachments"
+    ADD CONSTRAINT "FK_binary_contents_TO_message_attachments_1" FOREIGN KEY ("attachment_id")
+        REFERENCES "binary_contents" ("id") ON DELETE CASCADE;
+
+ALTER TABLE "user_statuses"
+    ADD CONSTRAINT "FK_users_TO_user_statuses_1" FOREIGN KEY ("user_id")
+        REFERENCES "users" ("id") ON DELETE CASCADE;
+
+ALTER TABLE "read_statuses"
+    ADD CONSTRAINT "FK_users_TO_read_statuses_1" FOREIGN KEY ("user_id")
+        REFERENCES "users" ("id") ON DELETE CASCADE;
+
+ALTER TABLE "read_statuses"
+    ADD CONSTRAINT "FK_channels_TO_read_statuses_1" FOREIGN KEY ("channel_id")
+        REFERENCES "channels" ("id") ON DELETE CASCADE;
