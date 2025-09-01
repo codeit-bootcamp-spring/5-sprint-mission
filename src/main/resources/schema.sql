44d29acf41da93b67a8828543777a55b78b28593
sooyeonz <5196ksy@naver.com>
2025-09-01 13:32:40 +0900

    feat: 데이터베이스 테이블 생성

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,78 @@
+CREATE TABLE binary_contents
+(
+    id           UUID PRIMARY KEY,
+    created_at   TIMESTAMPTZ  NOT NULL,
+    file_name    VARCHAR(255) NOT NULL,
+    size         BIGINT       NOT NULL,
+    content_type VARCHAR(100) NOT NULL,
+    bytes        BYTEA        NOT NULL
+);
+
+CREATE TABLE users
+(
+    id         UUID PRIMARY KEY,
+    created_at TIMESTAMPTZ         NOT NULL,
+    updated_at TIMESTAMPTZ,
+    username   VARCHAR(50) UNIQUE  NOT NULL,
+    email      VARCHAR(100) UNIQUE NOT NULL,
+    password   VARCHAR(60)         NOT NULL,
+    profile_id UUID,
+    CONSTRAINT users_profile_id_fk
+        FOREIGN KEY (profile_id) REFERENCES binary_contents (id) ON DELETE SET NULL
+);
+
+
+CREATE TABLE channels
+(
+    id          UUID PRIMARY KEY,
+    created_at  TIMESTAMPTZ NOT NULL,
+    updated_at  TIMESTAMPTZ,
+    name        VARCHAR(100),
+    description VARCHAR(500),
+    type        VARCHAR(10) NOT NULL CHECK ( type IN ('PUBLIC', 'PRIVATE'))
+);
+
+CREATE TABLE user_statuses
+(
+    id             UUID PRIMARY KEY,
+    created_at     TIMESTAMPTZ NOT NULL,
+    updated_at     TIMESTAMPTZ,
+    user_id        UUID        NOT NULL,
+    last_active_at TIMESTAMPTZ NOT NULL,
+    CONSTRAINT user_statuses_user_id_fk FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
+    CONSTRAINT user_id_uk UNIQUE (user_id)
+);
+
+CREATE TABLE messages
+(
+    id         UUID PRIMARY KEY,
+    created_at TIMESTAMPTZ NOT NULL,
+    updated_at TIMESTAMPTZ,
+    content    TEXT,
+    channel_id UUID        NOT NULL,
+    author_id  UUID,
+    CONSTRAINT messages_channel_id_fk FOREIGN KEY (channel_id) REFERENCES channels (id) ON DELETE CASCADE,
+    CONSTRAINT messages_author_id_fk FOREIGN KEY (author_id) REFERENCES users (id) ON DELETE SET NULL
+);
+
+CREATE TABLE read_statuses
+(
+    id           UUID PRIMARY KEY,
+    created_at   TIMESTAMPTZ NOT NULL,
+    updated_at   TIMESTAMPTZ,
+    user_id      UUID,
+    channel_id   UUID,
+    last_read_at TIMESTAMPTZ NOT NULL,
+    CONSTRAINT read_statuses_user_id_fk FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
+    CONSTRAINT read_statuses_channel_id_fk FOREIGN KEY (channel_id) REFERENCES channels (id) ON DELETE CASCADE,
+    CONSTRAINT read_statuses_uk UNIQUE (user_id, channel_id)
+);
+
+CREATE TABLE message_attachments
+(
+    message_id    UUID,
+    attachment_id UUID,
+    CONSTRAINT message_attachments_message_id_fk FOREIGN KEY (message_id) REFERENCES messages (id) ON DELETE CASCADE,
+    CONSTRAINT message_attachments_attachment_id_fk FOREIGN KEY (attachment_id) REFERENCES binary_contents (id) ON DELETE CASCADE
+);
+
