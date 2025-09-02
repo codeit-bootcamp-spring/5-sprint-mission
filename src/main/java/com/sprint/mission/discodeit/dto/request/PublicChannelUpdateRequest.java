243eaf335e4eb8884d39eaae294c25a94a49e884
rlawnsry <color9117@naver.com>
2025-09-02 15:16:23 +0900

    feat: sprint6 과제 - base code 적용

--- src/main/java/com/sprint/mission/discodeit/dto/request/PublicChannelUpdateRequest.java
+++ src/main/java/com/sprint/mission/discodeit/dto/request/PublicChannelUpdateRequest.java
@@ -1,8 +1,5 @@
 package com.sprint.mission.discodeit.dto.request;
 
-import io.swagger.v3.oas.annotations.media.Schema;
-
-@Schema(description = "수정할 Channel 정보")
 public record PublicChannelUpdateRequest(
     String newName,
     String newDescription
