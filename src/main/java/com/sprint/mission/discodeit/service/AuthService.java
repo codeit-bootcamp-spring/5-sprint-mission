c1e1d5f4134e16262ec14309a11d46e3efcc1041
JaehyeokLim <217686834+JaehyeokLim@users.noreply.github.com>
2025-09-02 17:42:33 +0900

    remove: Service Interface 모두 삭제

--- src/main/java/com/sprint/mission/discodeit/service/AuthService.java
+++
@@ -1,9 +0,0 @@
-package com.sprint.mission.discodeit.service;
-
-import com.sprint.mission.discodeit.dto.auth.request.LoginRequest;
-import com.sprint.mission.discodeit.dto.user.data.UserDto;
-import com.sprint.mission.discodeit.entity.main.User;
-
-public interface AuthService {
-    UserDto login(LoginRequest loginRequest);
-}
