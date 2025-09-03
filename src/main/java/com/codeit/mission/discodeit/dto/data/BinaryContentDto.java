d0cc9261cf3df0e61f3dd6d21b46afa5a3e290a5
sooyeonz <5196ksy@naver.com>
2025-09-03 15:13:46 +0900

    feat: data DTO 추가 및 수정

---
+++ src/main/java/com/codeit/mission/discodeit/dto/data/BinaryContentDto.java
@@ -0,0 +1,13 @@
+package com.codeit.mission.discodeit.dto.data;
+
+import java.util.UUID;
+
+public record BinaryContentDto(
+    UUID id,
+    String fileName,
+    Long size,
+    String contentType,
+    byte[] bytes
+) {
+
+}
--- src/main/java/com/codeit/mission/discodeit/dto/data/ChannelDto.java
+++ src/main/java/com/codeit/mission/discodeit/dto/data/ChannelDto.java
@@ -1,17 +1,17 @@
 package com.codeit.mission.discodeit.dto.data;
 
 import com.codeit.mission.discodeit.entity.ChannelType;
-
 import java.time.Instant;
 import java.util.List;
 import java.util.UUID;
 
 public record ChannelDto(
-        UUID id,
-        ChannelType type,
-        String name,
-        String description,
-        List<UUID> participantIds,
-        Instant lastMessageAt
+    UUID id,
+    ChannelType type,
+    String name,
+    String description,
+    List<UserDto> participants,
+    Instant lastMessageAt
 ) {
+
 }
