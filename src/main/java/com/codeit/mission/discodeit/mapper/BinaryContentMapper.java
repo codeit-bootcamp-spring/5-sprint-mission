555ea2a27ef7aea858f1e7ede5b77e720a1770a8
sooyeonz <5196ksy@naver.com>
2025-09-04 10:24:58 +0900

    feat: BinaryContentMapper 수정

--- src/main/java/com/codeit/mission/discodeit/mapper/BinaryContentMapper.java
+++ src/main/java/com/codeit/mission/discodeit/mapper/BinaryContentMapper.java
@@ -2,13 +2,29 @@ package com.codeit.mission.discodeit.mapper;
 
 import com.codeit.mission.discodeit.dto.data.BinaryContentDto;
 import com.codeit.mission.discodeit.entity.BinaryContent;
+import com.codeit.mission.discodeit.storage.BinaryContentStorage;
+import java.io.IOException;
+import java.io.InputStream;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class BinaryContentMapper {
 
+    private final BinaryContentStorage binaryContentStorage;
+
     public BinaryContentDto toDto(BinaryContent binaryContent) {
-        return new BinaryContentDto(binaryContent.getId(), binaryContent.getFileName(),
-            binaryContent.getSize(), binaryContent.getContentType(), binaryContent.getBytes());
+        try {
+            InputStream inputStream = binaryContentStorage.get(binaryContent.getId());
+            byte[] bytes = inputStream.readAllBytes();
+            inputStream.close();
+
+            return new BinaryContentDto(binaryContent.getId(), binaryContent.getFileName(),
+                binaryContent.getSize(), binaryContent.getContentType(), bytes);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
