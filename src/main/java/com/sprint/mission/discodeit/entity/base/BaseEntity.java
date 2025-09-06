5506e06d170d5a5096bf834a37f106d11589b8ac
polodumbo <polosh1208@gmail.com>
2025-09-07 07:19:12 +0900

    fix: 어노테이션 패키지 수정

--- src/main/java/com/sprint/mission/discodeit/entity/base/BaseEntity.java
+++ src/main/java/com/sprint/mission/discodeit/entity/base/BaseEntity.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 import java.time.Instant;
 import java.util.UUID;
@@ -12,7 +13,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.Id;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Getter
