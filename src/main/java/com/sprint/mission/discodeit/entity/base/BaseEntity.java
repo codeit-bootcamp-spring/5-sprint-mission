eb972e2520c536b833f5355915a7289b977ed4f0
truuuely <kmyc7030@gmail.com>
2025-09-08 00:48:09 +0900

    feat: JPA Auditing 적용 (@CreatedDate, @LastModifiedDate)

--- src/main/java/com/sprint/mission/discodeit/entity/base/BaseEntity.java
+++ src/main/java/com/sprint/mission/discodeit/entity/base/BaseEntity.java
@@ -1,13 +1,21 @@
 package com.sprint.mission.discodeit.entity.base;
 
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.Instant;
 import java.util.UUID;
 
 @Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity {
     private UUID id;
+
+    @CreatedDate
     private Instant createdAt;
 
     public BaseEntity() {
--- src/main/java/com/sprint/mission/discodeit/entity/base/BaseUpdatableEntity.java
+++ src/main/java/com/sprint/mission/discodeit/entity/base/BaseUpdatableEntity.java
@@ -1,14 +1,21 @@
 package com.sprint.mission.discodeit.entity.base;
 
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.Instant;
 
-
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public abstract class BaseUpdatableEntity extends BaseEntity {
-    @Getter
+
     @Setter
+    @LastModifiedDate
     private Instant updatedAt;
 
     public BaseUpdatableEntity() {
