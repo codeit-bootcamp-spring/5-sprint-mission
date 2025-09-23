07559b5143f29edeeb8487870d05d4cc08240298
polodumbo <polosh1208@gmail.com>
2025-09-24 05:57:59 +0900

    feat: Request DTO 제약 조건 관련 어노테이션 추가

--- src/main/java/com/sprint/mission/discodeit/dto/request/PublicChannelUpdateRequest.java
+++ src/main/java/com/sprint/mission/discodeit/dto/request/PublicChannelUpdateRequest.java
@@ -1,7 +1,12 @@
 package com.sprint.mission.discodeit.dto.request;
 
+import jakarta.validation.constraints.Size;
+
 public record PublicChannelUpdateRequest(
+    @Size(max = 100, message = "채널명은 100자 이하여야 합니다")
     String newName,
+
+    @Size(max = 500, message = "채널 설명은 500자 이하여야 합니다")
     String newDescription
 ) {
 
