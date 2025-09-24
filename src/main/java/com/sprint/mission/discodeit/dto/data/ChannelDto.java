8168b1563be2d5ae3b7cb32e6fd787a351bd393b
Haram Jo <harramaram@gmail.com>
2025-09-24 18:21:02 +0900

    feat: 유효성 어노테이션 추가

--- src/main/java/com/sprint/mission/discodeit/dto/data/ChannelDto.java
+++ src/main/java/com/sprint/mission/discodeit/dto/data/ChannelDto.java
@@ -1,6 +1,8 @@
 package com.sprint.mission.discodeit.dto.data;
 
 import com.sprint.mission.discodeit.entity.ChannelType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import java.util.UUID;
 import lombok.Data;
 
@@ -8,8 +10,14 @@ import lombok.Data;
 public class ChannelDto {
 
   private UUID id;
+
+  @NotBlank(message = "이름 입력은 필수입니다.")
   private String name;
+
+  @NotBlank(message = "설명 입력은 필수입니다.")
   private String description;
+
+  @NotNull(message = "채널 타입은 필수입니다.")
   private ChannelType channelType;
 
   //수정용 필드
--- src/main/java/com/sprint/mission/discodeit/dto/data/MessageDto.java
+++ src/main/java/com/sprint/mission/discodeit/dto/data/MessageDto.java
@@ -1,5 +1,7 @@
 package com.sprint.mission.discodeit.dto.data;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import java.util.UUID;
 import lombok.Data;
 
@@ -7,9 +9,14 @@ import lombok.Data;
 public class MessageDto {
 
   private UUID id;
+
+  @NotBlank(message = "메세지 내용은 필수입니다.")
   private String content;
 
+  @NotNull(message = "채널 ID는 필수입니다.")
   private UUID channelId;
+
+  @NotNull(message = "작성자 ID는 필수입니다.")
   private UUID authorId;
 
   // 업데이트용 필드
