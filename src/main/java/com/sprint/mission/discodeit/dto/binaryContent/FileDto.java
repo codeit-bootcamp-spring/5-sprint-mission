4924b2f9086910488470e1cab216b3fe3aedc9f4
이호준 <hj.lee@60hz.io>
2025-09-30 22:33:01 +0900

    merge conflict 해소 위한 초기화

--- src/main/java/com/sprint/mission/discodeit/dto/binaryContent/FileDto.java
+++
@@ -1,6 +0,0 @@
-package com.sprint.mission.discodeit.dto.binaryContent;
-
-public record FileDto(
-        String name,
-        String contentType,
-        byte[] content ) {}
--- src/main/java/com/sprint/mission/discodeit/dto/user/UserDto.java
+++
@@ -1,7 +0,0 @@
-package com.sprint.mission.discodeit.dto.user;
-
-public record UserDto (
-        String name,
-        String email,
-        String password
-) {}
