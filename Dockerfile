a1785440fe869d89e1e312e66e4c5128929b8258
parkjoohwan <jookid94@gmail.com>
2025-10-13 16:54:46 +0900

    feat : AWS S3 연동, Docker 세팅, 환경변수 분리

---
+++ Dockerfile
@@ -0,0 +1,38 @@
+# 1. 빌드 단계
+FROM amazoncorretto:17 as builder
+
+WORKDIR /app
+
+# Gradle Wrapper 파일 복사 (Maven 사용 시 mvnw로 교체)
+COPY gradlew .
+COPY gradle gradle
+COPY build.gradle settings.gradle ./
+
+RUN ./gradlew dependencies --no-daemon || true
+
+COPY src src
+RUN ./gradlew clean bootJar -x test
+#
+## 2. 실행 단계
+#FROM amazoncorretto:17
+#
+#WORKDIR /app
+#
+## 빌드된 JAR 복사
+#COPY --from=builder /app/build/libs/*.jar app.jar
+#
+#EXPOSE 8080
+#
+#ENTRYPOINT ["java", "-jar", "app.jar"]
+
+
+FROM eclipse-temurin:17-jre-jammy AS runtime
+WORKDIR /app
+
+ARG JAR_NAME=app.jar
+COPY --from=builder /app/build/libs/*.jar ${JAR_NAME}
+WORKDIR /app
+
+EXPOSE 8080
+
+ENTRYPOINT ["sh", "-c", "java $JVM_OPTS -jar /app/app.jar"]
---
+++ src/test/java/com/sprint/mission/discodeit/storage/AWSS3Test.java
@@ -0,0 +1,40 @@
+package com.sprint.mission.discodeit.storage;
+
+import com.sprint.mission.discodeit.config.AWSConfig;
+import java.io.InputStream;
+import java.util.UUID;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import software.amazon.awssdk.services.s3.S3Client;
+
+@SpringBootTest
+class AWSS3Test {
+
+  @Autowired
+  private AWSConfig config;
+
+  @Test
+  void testUpload() {
+    S3Client s3 = config.s3Client();
+    String key = "test/" + UUID.randomUUID() + ".txt";
+    s3.putObject(b -> b.bucket(config.getBucket())
+                       .key(key),
+        software.amazon.awssdk.core.sync.RequestBody.fromString("Hello S3!"));
+    System.out.println("✅ 업로드 성공: " + key);
+  }
+
+  @Test
+  void testDonwload() throws Exception {
+    S3Client s3 = config.s3Client();
+
+    String key = "test/test.txt";
+
+    InputStream is = s3.getObject(b -> b.bucket(config.getBucket())
+                                        .key(key));
+
+    System.out.println(is.readAllBytes()
+                         .toString());
+    System.out.println("✅ 다운로드 성공: " + key);
+  }
+}
