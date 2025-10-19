ceae23f760a08155008245f588880022108e7ba0
rlawnsry <color9117@naver.com>
2025-10-19 22:22:59 +0900

    chore: S3Properties에서 필요 없는 import문 삭제

--- src/main/java/com/sprint/mission/discodeit/storage/s3/S3Properties.java
+++ src/main/java/com/sprint/mission/discodeit/storage/s3/S3Properties.java
@@ -3,7 +3,6 @@ package com.sprint.mission.discodeit.storage.s3;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
