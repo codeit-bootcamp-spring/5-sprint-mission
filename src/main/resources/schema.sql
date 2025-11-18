f727e8b6a32f0a26a2fb88f8df2bd3ad2c9f3de4
parkjoohwan <jookid94@gmail.com>
2025-11-18 16:30:05 +0900

    fix : 테이블 쿼리 수정

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -28,6 +28,7 @@ CREATE TABLE users (
                        username VARCHAR(50) NOT NULL UNIQUE,
                        email VARCHAR(100) NOT NULL UNIQUE,
                        password VARCHAR(60) NOT NULL,
+                       role varchar(20) NOT NULL DEFAULT 'ROLE_USER',
                        profile_id UUID,
                        CONSTRAINT fk_users_profile FOREIGN KEY (profile_id)
                            REFERENCES binary_contents(id)
