68ab77854543c591872550d4b8804749dcd0f158
BakJiSeok <commicat2@gmail.com>
2025-11-19 22:12:38 +0900

    feat: Add UserControllerTest for user retrieval endpoint

---
+++ src/test/java/com/sprint/mission/discodeit/controller/UserControllerTest.java
@@ -0,0 +1,33 @@
+package com.sprint.mission.discodeit.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sprint.mission.discodeit.service.UserService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(UserController.class)
+public class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockitoBean
+    private UserService userService;
+
+    @Test
+    @DisplayName("GET /api/users - 성공 (200 OK): 유저 목록 조회")
+    void findAll_success() throws Exception {
+        // given
+
+        // when
+
+        // then
+    }
+}
