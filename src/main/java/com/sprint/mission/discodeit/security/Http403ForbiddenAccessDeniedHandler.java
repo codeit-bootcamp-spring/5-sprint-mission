53d5e52a59da2bd73df3e1f602bc236f3ab18fe0
rlawnsry <color9117@naver.com>
2025-11-20 17:21:22 +0900

    feat: 인가 - 권한 적용 요구사항 적용

---
+++ src/main/java/com/sprint/mission/discodeit/security/Http403ForbiddenAccessDeniedHandler.java
@@ -0,0 +1,31 @@
+package com.sprint.mission.discodeit.security;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sprint.mission.discodeit.exception.ErrorResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class Http403ForbiddenAccessDeniedHandler implements AccessDeniedHandler {
+
+  private final ObjectMapper objectMapper;
+
+  @Override
+  public void handle(HttpServletRequest request, HttpServletResponse response,
+      AccessDeniedException accessDeniedException) throws IOException, ServletException {
+    response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+    response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+    ErrorResponse errorResponse = new ErrorResponse(accessDeniedException,
+        HttpStatus.FORBIDDEN.value());
+    response.getWriter().write(objectMapper.writeValueAsString(errorResponse));
+  }
+}
--- src/main/java/com/sprint/mission/discodeit/service/basic/BasicChannelService.java
+++ src/main/java/com/sprint/mission/discodeit/service/basic/BasicChannelService.java
@@ -18,6 +18,7 @@ import com.sprint.mission.discodeit.service.ChannelService;
 import java.util.List;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import lombok.extern.slf4j.Slf4j;
@@ -34,6 +35,7 @@ public class BasicChannelService implements ChannelService {
   private final UserRepository userRepository;
   private final ChannelMapper channelMapper;
 
+  @PreAuthorize("hasRole('CHANNEL_MANAGER')")
   @Transactional
   @Override
   public ChannelDto create(PublicChannelCreateRequest request) {
@@ -47,6 +49,7 @@ public class BasicChannelService implements ChannelService {
     return channelMapper.toDto(channel);
   }
 
+  @PreAuthorize("hasRole('CHANNEL_MANAGER')")
   @Transactional
   @Override
   public ChannelDto create(PrivateChannelCreateRequest request) {
