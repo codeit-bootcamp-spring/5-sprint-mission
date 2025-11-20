b58323003620ca08a64c2e6a3335581d2e38e168
rlawnsry <color9117@naver.com>
2025-11-20 10:19:10 +0900

    feat: AuthController에 api/auth/me 엔드포인트 추가

--- src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
@@ -1,11 +1,14 @@
 package com.sprint.mission.discodeit.controller;
 
 import com.sprint.mission.discodeit.controller.api.AuthApi;
+import com.sprint.mission.discodeit.dto.data.UserDto;
+import com.sprint.mission.discodeit.security.DiscodeitUserDetails;
 import com.sprint.mission.discodeit.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +21,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController implements AuthApi {
 
   private final AuthService authService;
+
+  @GetMapping("me")
+  public ResponseEntity<UserDto> me(@AuthenticationPrincipal DiscodeitUserDetails userDetails) {
+    log.info("내 정보 조회");
+    return ResponseEntity.status(HttpStatus.OK).body(userDetails.getUserDto());
+  }
 
   @GetMapping("csrf-token")
   public ResponseEntity<CsrfToken> getCsrfToken(CsrfToken csrfToken) {
--- src/main/java/com/sprint/mission/discodeit/controller/api/AuthApi.java
+++ src/main/java/com/sprint/mission/discodeit/controller/api/AuthApi.java
@@ -1,11 +1,16 @@
 package com.sprint.mission.discodeit.controller.api;
 
+import com.sprint.mission.discodeit.dto.data.UserDto;
+import com.sprint.mission.discodeit.security.DiscodeitUserDetails;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.web.csrf.CsrfToken;
 
 @Tag(name = "Auth", description = "인증 API")
 public interface AuthApi {
+
+  ResponseEntity<UserDto> me(@AuthenticationPrincipal DiscodeitUserDetails userDetails);
 
   ResponseEntity<CsrfToken> getCsrfToken(CsrfToken csrfToken);
 } 
