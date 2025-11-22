6f9596139e09f02a83488459091103ea77a69130
BakJiSeok <commicat2@gmail.com>
2025-11-22 19:37:33 +0900

    chore

--- src/test/java/com/sprint/mission/discodeit/controller/UserControllerTest.java
+++ src/test/java/com/sprint/mission/discodeit/controller/UserControllerTest.java
@@ -6,13 +6,13 @@ import com.sprint.mission.discodeit.service.UserService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.context.annotation.Import;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(UserController.class)
-@Import(ErrorHandlingProperties.class)
+@EnableConfigurationProperties(ErrorHandlingProperties.class)
 public class UserControllerTest {
 
     @Autowired
