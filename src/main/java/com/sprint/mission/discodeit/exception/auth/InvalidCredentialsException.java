daf045ddbfb7bf313f091af536416f367091a511
BakJiSeok <commicat2@gmail.com>
2025-11-22 20:43:50 +0900

    refactor: rename error code for invalid credentials and update exception handling

--- src/main/java/com/sprint/mission/discodeit/exception/auth/InvalidCredentialsException.java
+++ src/main/java/com/sprint/mission/discodeit/exception/auth/InvalidCredentialsException.java
@@ -5,6 +5,6 @@ import com.sprint.mission.discodeit.exception.ErrorCode;
 public class InvalidCredentialsException extends AuthException {
 
     public InvalidCredentialsException() {
-        super(ErrorCode.WRONG_USERNAME_OR_PASSWORD);
+        super(ErrorCode.INVALID_CREDENTIALS);
     }
 }
