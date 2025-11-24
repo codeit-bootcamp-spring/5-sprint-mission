3d322ab1c9d4b8ee78c41d59734e3fadab4c584a
JaehyeokLim <217686834+JaehyeokLim@users.noreply.github.com>
2025-11-24 13:34:54 +0900

    feat: Role 추가

--- src/main/java/com/sprint/mission/discodeit/entity/User.java
+++ src/main/java/com/sprint/mission/discodeit/entity/User.java
@@ -1,42 +1,53 @@
 package com.sprint.mission.discodeit.entity;
 
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.sprint.mission.discodeit.entity.base.BaseUpdatableEntity;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Entity
 @Table(name = "users")
 @Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)  // JPA를 위한 기본 생성자
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User extends BaseUpdatableEntity {
 
   @Column(length = 50, nullable = false, unique = true)
   private String username;
+
   @Column(length = 100, nullable = false, unique = true)
   private String email;
+
   @Column(length = 60, nullable = false)
   private String password;
+
   @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
   @JoinColumn(name = "profile_id", columnDefinition = "uuid")
   private BinaryContent profile;
-  @JsonManagedReference
-  @Setter(AccessLevel.PROTECTED)
-  @OneToOne(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
-  private UserStatus status;
 
-  public User(String username, String email, String password, BinaryContent profile) {
+  @Enumerated(EnumType.STRING)
+  private Role role;
+
+  private User(String username, String email, String password, BinaryContent profile) {
     this.username = username;
     this.email = email;
     this.password = password;
     this.profile = profile;
   }
 
-  public void update(String newUsername, String newEmail, String newPassword,
-      BinaryContent newProfile) {
+  public static User createUser(String username, String email, String password, BinaryContent profile) {
+    User user = new User(username, email, password, profile);
+    user.role = Role.USER;
+    return user;
+  }
+
+  public static User createAdmin(String username, String email, String password, BinaryContent profile) {
+    User user = new User(username, email, password, profile);
+    user.role = Role.ADMIN;
+    return user;
+  }
+
+  public void update(String newUsername, String newEmail, String newPassword, BinaryContent newProfile) {
     if (newUsername != null && !newUsername.equals(this.username)) {
       this.username = newUsername;
     }
@@ -49,5 +60,9 @@ public class User extends BaseUpdatableEntity {
     if (newProfile != null) {
       this.profile = newProfile;
     }
+  }
+
+  public void updateRole(Role newRole) {
+    this.role = newRole;
   }
 }
