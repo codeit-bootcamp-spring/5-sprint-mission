eb7f12f482a26b0e34c569883fd6ef1e9d70e0ce
JaehyeokLim <217686834+JaehyeokLim@users.noreply.github.com>
2025-11-24 13:39:54 +0900

    feat: 인증을 위한 커스텀 DB UserDetails 추가

---
+++ src/main/java/com/sprint/mission/discodeit/security/DiscodeitUserDetailsService.java
@@ -0,0 +1,28 @@
+package com.sprint.mission.discodeit.security;
+
+import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.mapper.UserMapper;
+import com.sprint.mission.discodeit.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class DiscodeitUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException(username));
+
+        return new DiscodeitUserDetails(userMapper.toDto(user), user.getPassword());
+    }
+}
