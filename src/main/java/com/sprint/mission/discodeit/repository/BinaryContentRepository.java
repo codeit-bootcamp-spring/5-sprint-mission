a2391470059a19ceb7a1e01b64608acbadf6fa4d
BakJiSeok <commicat2@gmail.com>
2025-11-25 13:28:04 +0900

    feat: enhance validation in BinaryContent and update error codes for user not found scenarios

--- src/main/java/com/sprint/mission/discodeit/repository/BinaryContentRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/BinaryContentRepository.java
@@ -4,8 +4,12 @@ import com.sprint.mission.discodeit.entity.BinaryContent;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
 
 @ConditionalOnProperty(prefix = "discodeit.storage", name = "type", havingValue = "local")
 public interface BinaryContentRepository extends JpaRepository<BinaryContent, UUID> {
+
+    List<BinaryContent> findAllByIdIn(Collection<UUID> ids);
 }
