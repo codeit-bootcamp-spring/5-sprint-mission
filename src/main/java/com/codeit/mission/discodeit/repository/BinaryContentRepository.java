92a908e149b3c76639e642c25ea457182845a261
sooyeonz <5196ksy@naver.com>
2025-11-28 11:06:05 +0900

    fix: 스프린트9 베이스코드로 변경

--- src/main/java/com/codeit/mission/discodeit/repository/BinaryContentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.codeit.mission.discodeit.repository;
-
-import com.codeit.mission.discodeit.entity.BinaryContent;
-import java.util.UUID;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BinaryContentRepository extends JpaRepository<BinaryContent, UUID> {
-
-}
