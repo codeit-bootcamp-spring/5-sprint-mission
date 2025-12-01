bbb361b58c41cbea6e93516dcc7ade3527603c26
김규섭 <pens9053@gmail.com>
2025-12-01 17:44:46 +0900

    refactor: 파일 저장 성공 여부를 알 수 있도록 메타데이터 리팩토링

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -27,7 +27,8 @@ CREATE TABLE binary_contents
     created_at   TIMESTAMP WITH TIME ZONE NOT NULL,
     file_name    VARCHAR(255)             NOT NULL,
     size         BIGINT                   NOT NULL,
-    content_type VARCHAR(100)             NOT NULL
+    content_type VARCHAR(100)             NOT NULL,
+    status       VARCHAR(20)              NOT NULL
 --     ,bytes        bytea        NOT NULL
 );
 
