a596e8c3c4dcd75f2c782ad2de8bda566d38f2d2
BakJiSeok <commicat2@gmail.com>
2025-12-04 23:09:45 +0900

    refactor(exception): remove unused constructors from InsufficientRoleException

--- src/main/java/com/sprint/mission/discodeit/common/exception/auth/InsufficientRoleException.java
+++ src/main/java/com/sprint/mission/discodeit/common/exception/auth/InsufficientRoleException.java
@@ -2,19 +2,9 @@ package com.sprint.mission.discodeit.common.exception.auth;
 
 import com.sprint.mission.discodeit.common.exception.ErrorCode;
 
-import java.util.Map;
-
 public class InsufficientRoleException extends AuthException {
 
     public InsufficientRoleException() {
         super(ErrorCode.INSUFFICIENT_ROLE);
-    }
-
-    public InsufficientRoleException(String requiredRole) {
-        super(ErrorCode.INSUFFICIENT_ROLE, Map.of("requiredRole", requiredRole));
-    }
-
-    public InsufficientRoleException(String requiredRole, Throwable cause) {
-        super(ErrorCode.INSUFFICIENT_ROLE, Map.of("requiredRole", requiredRole), cause);
     }
 }
