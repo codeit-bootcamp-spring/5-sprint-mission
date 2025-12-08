d262f0086a86690d888de07a744a902ee88373c8
BakJiSeok <commicat2@gmail.com>
2025-12-08 16:43:22 +0900

    refactor: remove logging from BinaryContentEventListener for BinaryContentCreatedEvent

--- src/main/java/com/sprint/mission/discodeit/binarycontent/application/BinaryContentEventListener.java
+++ src/main/java/com/sprint/mission/discodeit/binarycontent/application/BinaryContentEventListener.java
@@ -2,14 +2,12 @@ package com.sprint.mission.discodeit.binarycontent.application;
 
 import com.sprint.mission.discodeit.binarycontent.domain.BinaryContentCreatedEvent;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionalEventListener;
 
 @Component
 @RequiredArgsConstructor
-@Slf4j
 public class BinaryContentEventListener {
 
     private final BinaryContentStorageProcessor storageProcessor;
@@ -17,8 +15,6 @@ public class BinaryContentEventListener {
     @Async
     @TransactionalEventListener
     public void on(BinaryContentCreatedEvent event) {
-        log.info("Received BinaryContentCreatedEvent for binaryContentId: {}", event.binaryContentId());
-
         storageProcessor.processWithRetry(event);
     }
 }
